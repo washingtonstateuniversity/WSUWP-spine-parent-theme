7e9580474fd9469e576a30afab4ad763ee888ff0
Nate Jacobson <nate.jacobson@wsu.edu>
2013-11-22 16:47:05 -0800

    Footer gobbledy.
    Seems I need to suss out Git's reconciler for Diffs.

--- footer.php
+++ footer.php
@@ -1,11 +1,7 @@
 </div><!--/cover-->
 </div><!--/jacket-->
-<<<<<<< HEAD
 
 <?php wp_footer(); ?>
 
-=======
-<?php wp_footer(); ?>
->>>>>>> a7a88ac449d7b70fedc6ef96530b7fb767f41144
 </body>
 </html>
