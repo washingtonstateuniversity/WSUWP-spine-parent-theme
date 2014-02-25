634414a1e1d29530b070a6b8e123bf52706b01ca
Nate Jacobson <nate.jacobson@wsu.edu>
2014-02-25 12:20:26 -0800

    Move while into parent template

--- articles/article.php
+++ articles/article.php
@@ -1,11 +1,8 @@
 <?php // Just a stub for now ?>
-<?php while ( have_posts() ) : the_post(); ?>
-				
+		
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="article-header">
 		<h1 class="article-title"><?php the_title(); ?></h1>
 	</header>
 	<?php the_content(); ?>
 </article>
-
-<?php endwhile; ?>
