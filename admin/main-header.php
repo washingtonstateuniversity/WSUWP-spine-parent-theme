1c220639e7d7c5c6ccc653ccc62c6c25cc215dd2
jeremyfelt <jeremy.felt@gmail.com>
2014-04-09 10:08:14 -0700

    Refactor handling of sup and sub around missing page_for_posts
    WordPress has no page assigned for posts by default, so we'll need
    to work around that for determining proper sub and sub headings.

--- admin/main-header.php
+++ admin/main-header.php
@@ -1,74 +1,123 @@
 <?php
 
 function spine_get_main_header() {
-	global $post;
-	$site_name          = get_bloginfo('name','display');
-	$site_tagline       = get_bloginfo('description','display');
+	$page_for_posts = absint( get_option( 'page_for_posts', 0 ) );
+
+	if ( 0 !== $page_for_posts ) {
+		$posts_page_title = get_the_title( $page_for_posts );
+	} else {
+		$posts_page_title = '';
+	}
+
+	$site_name          = get_bloginfo( 'name', 'display' );
+	$site_tagline       = get_bloginfo( 'description', 'display' );
 	$page_title	        = get_the_title();
 	$post_title         = get_the_title();
-	$section_title      = spine_section_meta('title','section');
-	$subsection_title   = spine_section_meta('title','subsection');
-	$posts_page_title   = get_the_title( get_option('page_for_posts', true) );
+	$section_title      = spine_section_meta( 'title', 'section' );
+	$subsection_title   = spine_section_meta( 'title', 'subsection' );
 
-	$sup_header_default	  = '<a href="'.home_url('/').'" rel="home">'.$site_name.'</a>';
-	$sub_header_default   = spine_section_meta('title','subsection');
+	$sup_header_default	  = '<a href="' . home_url( '/' ) . '" rel="home">' . $site_name . '</a>';
+	$sub_header_default   = spine_section_meta( 'title', 'subsection' );
 	$sup_header_alternate = '';
 	$sub_header_alternate = '';
 
-	// Posts
+	// On a category archive view, use the page title of the page_for_posts setting as the sup
+	// header if available, otherwise fallback to the site name.
 	if ( is_category() ) {
-		$sup_link = get_permalink( get_option('page_for_posts') );
-		$sup_header_default = '<a href="'.$sup_link.'">'.$posts_page_title.'</a>';
-		$sub_header_default = single_cat_title('', false);
-		$section_title = $posts_page_title;
-	}
-	if ( is_archive() && !is_category() ) {
-		if ( is_day() ) : $sub_header_default = get_the_date();
-			elseif ( is_month() ) : $sub_header_default = get_the_date( 'F Y' );
-			elseif ( is_year() )  : $sub_header_default = get_the_date( 'Y' );
-			else : $sub_header_default = 'Archives';
-			endif;
-		$sup_header_default = $posts_page_title;
-		$section_title = $posts_page_title;
-	}
-	if ( is_single() ) {
-		$sub_header_default = $posts_page_title;
-	}
-
-	// Pages
-	if ( is_page() ) {
-		$sub_header_default = $page_title;
-		if ( spine_is_sub() ) {
-			// $sup_header_default = $section_title;
-			$sub_link = get_permalink($post->post_parent);
-			$sub_header_default = '<a href="'.$sub_link.'">'.$subsection_title.'</a>';
+		if ( 0 === $page_for_posts ) {
+			$sup_header_default = '<a href="' . esc_url( home_url() ) . '">' . esc_html( $site_name ) . '</a>';
+			$sub_header_default = single_cat_title( '', false );
+			$section_title = $site_name;
+		} else {
+			$sup_link = get_permalink( $page_for_posts );
+			$sup_header_default = '<a href="' . esc_url( $sup_link ) . '">' . $posts_page_title . '</a>';
+			$sub_header_default = single_cat_title( '', false );
+			$section_title = $posts_page_title;
 		}
 	}
+
+	// On date archive views, use one of the day, month, year as the sub header. If a tag or other
+	// non category archive, use 'Archives'. Use the page title of page_for_posts if available as the
+	// sup header, otherwise use the site name.
+	if ( is_archive() && ! is_category() ) {
+		if ( is_day() ) {
+			$sub_header_default = get_the_date();
+		} else if ( is_month() ) {
+			$sub_header_default = get_the_date( 'F Y' );
+		} else if ( is_year() ) {
+			$sub_header_default = get_the_date( 'Y' );
+		} else {
+			$sub_header_default = 'Archives';
+		}
+
+		if ( 0 === $page_for_posts ) {
+			$sup_header_default = $site_name;
+			$section_title = $site_name;
+		} else {
+			$sup_header_default = $posts_page_title;
+			$section_title = $posts_page_title;
+		}
+	}
+
+	// @todo If page_for_posts is not set, sup and sub are the same (?)
+	if ( is_single() ) {
+		if ( 0 === $page_for_posts ) {
+			$sub_header_default = $site_name;
+		} else {
+			$sub_header_default = $posts_page_title;
+		}
+	}
+
+	// If this page is a child of another page, use the subsection title as a sub
+	// header. Otherwise, use the current page's title.
+	if ( is_page() ) {
+		if ( spine_is_sub() ) {
+			$post = get_post();
+			$sub_link = get_permalink( $post->post_parent );
+			$sub_header_default = '<a href="' . $sub_link . '">' . $subsection_title . '</a>';
+		} else {
+			$sub_header_default = $page_title;
+		}
+	}
+
 	if ( is_front_page() && is_page() ) {
 		$sub_header_default = $site_tagline;
 	}
-	if ( is_home() && !is_front_page() ) {
+
+	// @todo If page_for_posts is not set, sup and sub are the same (?)
+	if ( is_home() && ! is_front_page() ) {
 		$sup_header_default = $site_name;
-		$sub_header_default = $posts_page_title;
-		$page_title = $posts_page_title;
+
+		if ( 0 === $page_for_posts ) {
+			$page_title = $site_name;
+			$sub_header_default = $site_name;
+		} else {
+			$sub_header_default = $posts_page_title;
+			$page_title = $posts_page_title;
+		}
 	}
 
-	// Search and 404
 	if ( is_search() ) {
 		$sup_header_alternate = 'Search Terms';
 		$sub_header_default = 'Search Results';
-		$sub_header_alternate = get_search_query();
+		$sub_header_alternate = esc_html( get_search_query() );
 	}
+
 	if ( is_404() ) {
 		$sub_header_default = 'Page not found';
 	}
 
-	// Custom Field Overrides
+	// Both sup and sub headers can be overridden with the use of post meta.
 	if ( is_singular() ) {
-		$sup_override = get_post_meta(get_the_ID(), 'sup-header', true);
-		$sub_override = get_post_meta(get_the_ID(), 'sub-header', true);
-		if ( $sup_override != '' ) { $sup_header_default = $sup_override; }
-		if ( $sub_override != '' ) { $sub_header_default = $sub_override; }
+		$sup_override = get_post_meta( get_the_ID(), 'sup-header', true );
+		$sub_override = get_post_meta( get_the_ID(), 'sub-header', true );
+
+		if ( ! empty( $sup_override ) ) {
+			$sup_header_default = wp_kses_post( $sup_override );
+		}
+		if ( ! empty( $sub_override ) ) {
+			$sub_header_default = wp_kses_post( $sub_override );
+		}
 	}
 
 	return array(
