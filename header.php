40e0283c65e5315cbda9de0af6cfe35196f0fb7a
Nate Jacobson <nate.jacobson@wsu.edu>
2014-04-14 16:38:35 -0700

    Change to help for validation

--- header.php
+++ header.php
@@ -41,7 +41,7 @@
 	
 	<!-- DOCS -->
 	<link type="text/plain" rel="author" href="//repo.wsu.edu/spine/1/authors.txt" />
-	<link type="text/html" rel="docs" href="http://brand.wsu.edu/media/web" />
+	<link type="text/html" rel="help" href="http://brand.wsu.edu/media/web" />
 	
 	<!-- ANALYTICS -->
 	<!-- Your analytics code here -->
