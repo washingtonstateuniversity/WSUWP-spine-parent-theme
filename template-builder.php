a7041e5f4a0640d4f95bcec2660d5da9bc9b5f09
jeremyfelt <jeremy.felt@gmail.com>
2014-05-14 16:23:06 -0700

    Provide intent for the removal of the_content filter

--- template-builder.php
+++ template-builder.php
@@ -1,21 +1,27 @@
 <?php
 /**
  * Template Name: Builder Template
- *
- * @package ttf-one
  */
 
 get_header();
 ?>
-
 	<main class="spine-blank-template">
 
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 			<div id="page-<?php the_ID(); ?>" <?php post_class(); ?>>
-				<?php remove_filter( 'the_content', 'wpautop', 10 ); ?>
-				<?php the_content(); ?>
-				<?php add_filter( 'the_content', 'wpautop', 10 ); ?>
+				<?php
+
+				/**
+				 * `the_content` is fired on builder template pages while it is saved
+				 * rather than while it is output in order for some advanced tags to
+				 * survive the process and to avoid autop issues.
+				 */
+				remove_filter( 'the_content', 'wpautop', 10 );
+				the_content();
+				add_filter( 'the_content', 'wpautop', 10 );
+
+				?>
 			</div><!-- #post -->
 
 		<?php endwhile; endif; ?>
