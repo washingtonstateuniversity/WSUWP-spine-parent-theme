2c2283faaa476fdc7806b58255f765a7bc3bea75
Nathan Jacobson <nate.jacobson@wsu.edu>
2014-05-15 17:47:29 -0700

    Pull layout specific styles from main styles.css

--- templates/margin.php
+++ templates/margin.php
@@ -2,7 +2,7 @@
 
 <?php get_header(); ?>
 
-<main class="spine-sideright-template">
+<main class="spine-margin-template spine-margin-right-template">
 
 <?php if ( have_posts() ) : while( have_posts() ) : the_post(); ?>
 
