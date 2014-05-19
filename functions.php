83217097d966cd637bd35adce9dc66611648b522
Nathan Jacobson <nate.jacobson@wsu.edu>
2014-05-19 16:45:13 -0700

    Add custom spine excerpt

--- functions.php
+++ functions.php
@@ -76,7 +76,7 @@ function spine_get_option( $option_name ) {
 		'large_format'              => '',
 		'theme_style'               => 'bookmark',
 		'broken_binding'            => false,
-		'bleed'                     => false,
+		'bleed'                     => true,
 		'open_sans'                 => false,
 		'contact_name'              => 'Washington State University',
 		'contact_department'        => '',
@@ -263,18 +263,59 @@ function spine_abbridged_menu_classes( $classes ) {
 	return array();
 }
 
+// Custom Excerpt
+function spine_trim_excerpt($text) {
+$raw_excerpt = $text;
+if ( '' == $text ) {
+    //Retrieve the post content. 
+    $text = get_the_content('');
+ 
+    //Delete all shortcode tags from the content. 
+    $text = strip_shortcodes( $text );
+ 
+    $text = apply_filters('the_content', $text);
+    $text = str_replace(']]>', ']]&gt;', $text);
+     
+    $allowed_tags = '<p>,<a>,<em>,<strong>,<img>';
+    $text = strip_tags($text, $allowed_tags);
+     
+    $excerpt_word_count = 105;
+    $excerpt_length = apply_filters('excerpt_length', $excerpt_word_count); 
+     
+    $excerpt_end = '<a href="'. get_permalink($post->ID) . '">' . '&raquo; More ...' . '</a>';
+    $excerpt_more = apply_filters('excerpt_more', ' ' . $excerpt_end);
+     
+    $words = preg_split("/[\n\r\t ]+/", $text, $excerpt_length + 1, PREG_SPLIT_NO_EMPTY);
+    if ( count($words) > $excerpt_length ) {
+        array_pop($words);
+        $text = implode(' ', $words);
+        $text = $text . $excerpt_more;
+    } else {
+        $text = implode(' ', $words);
+    }
+}
+return apply_filters('wp_trim_excerpt', $text, $raw_excerpt);
+}
+remove_filter('get_the_excerpt', 'wp_trim_excerpt');
+add_filter('get_the_excerpt', 'spine_trim_excerpt');
+
+
+/* @jeremyfelt, I'll leave it to you to convert these from 'update_option' to merely defaults. */
 add_action( 'admin_init', 'spine_theme_image_options' );
 function spine_theme_image_options() {
 	// Default Image Sizes
 	update_option( 'thumbnail_size_w', 198   );
 	update_option( 'thumbnail_size_h', 198   );
 	update_option( 'medium_size_w',    396   );
-	update_option( 'medium_size_h',    99163 );
+	update_option( 'medium_size_h',    99164 );
 	update_option( 'large_size_w',     792   );
-	update_option( 'large_size_h',     99163 );
+	update_option( 'large_size_h',     99164 );
 	// update_option('full_size_w', 1980);
 	// update_option('full_size_h', 99163);
+	add_image_size( 'spine-thumbnail_size', 198, 198, true );
+	add_image_size( 'spine-medium_size', 396, 99164 );
 }
+
 
 /* Default Image Markup */
 
