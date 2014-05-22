8dcfd7833b1a77394bc6023b09e6ebf34dbd107e
jeremyfelt <jeremy.felt@gmail.com>
2014-05-22 12:16:45 -0700

    Update the sidebar left template with Spine defaults

--- builder-templates/front-end/sidebar-left.php
+++ builder-templates/front-end/sidebar-left.php
@@ -2,7 +2,6 @@
 global $ttfmake_section_data, $ttfmake_sections;
 $sideleft_columns = spine_get_two_column_data( $ttfmake_section_data );
 ?>
-
 <section id="builder-section-<?php echo esc_attr( $ttfmake_section_data['id'] ); ?>" class="row sideleft">
 		<?php $count = 'one'; ?>
 		<?php if ( ! empty( $sideleft_columns ) ) : foreach ( $sideleft_columns as $column ) :
@@ -13,22 +12,24 @@ $sideleft_columns = spine_get_two_column_data( $ttfmake_section_data );
 				$link_back = '</a>';
 			endif;
 			?>
-		<div class="column <?php echo $count; $count = 'two'; ?> builder-text-column">
-			<?php if ( 0 !== absint( $column['image-id'] ) ) : ?>
-			<figure class="builder-sideleft-image">
-				<?php echo $link_front . wp_get_attachment_image( $column['image-id'], 'large' ) . $link_back; ?>
-			</figure>
-			<?php endif; ?>
-			<?php if ( '' !== $column['title'] ) : ?>
-			<h3 class="builder-sideleft-title">
-				<?php echo apply_filters( 'the_title', $column['title'] ); ?>
-			</h3>
-			<?php endif; ?>
-			<?php if ( '' !== $column['content'] ) : ?>
-			<div class="builder-sideleft-content">
-				<?php ttfmake_get_builder_save()->the_builder_content( $column['content'] ); ?>
-			</div>
-			<?php endif; ?>
+		<div class="column <?php echo $count; $count = 'two'; ?>">
+			<article>
+				<?php if ( 0 !== absint( $column['image-id'] ) ) : ?>
+				<figure class="builder-sideleft-image">
+					<?php echo $link_front . wp_get_attachment_image( $column['image-id'], 'large' ) . $link_back; ?>
+				</figure>
+				<?php endif; ?>
+
+				<?php if ( '' !== $column['title'] ) : ?>
+				<header>
+					<h2><?php echo apply_filters( 'the_title', $column['title'] ); ?></h2>
+				</header>
+				<?php endif; ?>
+
+				<?php if ( '' !== $column['content'] ) : ?>
+					<?php ttfmake_get_builder_save()->the_builder_content( $column['content'] ); ?>
+				<?php endif; ?>
+			</article>
 		</div>
 		<?php endforeach; endif; ?>
 </section>
