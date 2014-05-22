4ec1466215395efe59f36b1318a1340188a9d648
jeremyfelt <jeremy.felt@gmail.com>
2014-05-22 12:11:37 -0700

    Modify the default single template to better match Spine defaults

--- builder-templates/front-end/single.php
+++ builder-templates/front-end/single.php
@@ -2,24 +2,20 @@
 global $ttfmake_section_data, $ttfmake_sections;
 ?>
 
-<section id="builder-section-<?php echo esc_attr( $ttfmake_section_data['id'] ); ?>" class="single row">
+<section id="builder-section-<?php echo esc_attr( $ttfmake_section_data['id'] ); ?>" class="row single">
 	<div class="column one">
+		<article>
+
 		<?php if ( ! empty( $ttfmake_section_data['title'] ) ) : ?>
-			<header class="builder-section-header">
-				<h3 class="builder-section-title">
-					<?php echo apply_filters( 'the_title', $ttfmake_section_data['title'] ); ?>
-				</h3>
+			<header>
+				<h2><?php echo apply_filters( 'the_title', $ttfmake_section_data['title'] ); ?></h2>
 			</header>
 		<?php endif; ?>
 
 		<?php if ( ! empty( $ttfmake_section_data['content'] ) ) : ?>
-			<div class="builder-section-content">
-				<?php if ( '' !== $ttfmake_section_data['content'] ) : ?>
-					<div class="builder-blank-content">
-						<?php ttfmake_get_builder_save()->the_builder_content( $ttfmake_section_data['content'] ); ?>
-					</div>
-				<?php endif; ?>
-			</div>
+			<?php ttfmake_get_builder_save()->the_builder_content( $ttfmake_section_data['content'] ); ?>
 		<?php endif; ?>
+
+		</article>
 	</div>
 </section>
