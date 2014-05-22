18a4cadf44bf6b3aeea1782c9d9197b54fe8177d
jeremyfelt <jeremy.felt@gmail.com>
2014-05-22 12:18:53 -0700

    Update the sidebar right front end template for Spine defaults

--- builder-templates/front-end/sidebar-right.php
+++ builder-templates/front-end/sidebar-right.php
@@ -2,7 +2,6 @@
 global $ttfmake_section_data, $ttfmake_sections;
 $text_columns = spine_get_two_column_data( $ttfmake_section_data );
 ?>
-
 <section id="builder-section-<?php echo esc_attr( $ttfmake_section_data['id'] ); ?>" class="row sidebar">
 	<?php $count = 'one'; ?>
 	<?php if ( ! empty( $text_columns ) ) : foreach ( $text_columns as $column ) :
@@ -13,22 +12,26 @@ $text_columns = spine_get_two_column_data( $ttfmake_section_data );
 			$link_back = '</a>';
 		endif;
 		?>
-		<div class="column <?php echo $count; $count = 'two'; ?> builder-text-column">
+		<div class="column <?php echo $count; $count = 'two'; ?>">
+			<article>
+
 			<?php if ( 0 !== absint( $column['image-id'] ) ) : ?>
 				<figure class="builder-text-image">
 					<?php echo $link_front . wp_get_attachment_image( $column['image-id'], 'large' ) . $link_back; ?>
 				</figure>
 			<?php endif; ?>
+
 			<?php if ( '' !== $column['title'] ) : ?>
-				<h3 class="builder-text-title">
-					<?php echo apply_filters( 'the_title', $column['title'] ); ?>
-				</h3>
+				<header>
+					<h2><?php echo apply_filters( 'the_title', $column['title'] ); ?></h2>
+				</header>
 			<?php endif; ?>
+
 			<?php if ( '' !== $column['content'] ) : ?>
-				<div class="builder-text-content">
-					<?php ttfmake_get_builder_save()->the_builder_content( $column['content'] ); ?>
-				</div>
+				<?php ttfmake_get_builder_save()->the_builder_content( $column['content'] ); ?>
 			<?php endif; ?>
+
+			</article>
 		</div>
 	<?php endforeach; endif; ?>
 </section>
