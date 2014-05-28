b0fdfc0aad72aa1751e2a0bdf6d2d7b61923a1c8
Nathan Jacobson <nate.jacobson@wsu.edu>
2014-05-28 07:27:30 -0700

    Add now needed classes to action tabs

--- spine/header.php
+++ spine/header.php
@@ -5,10 +5,10 @@
 <section id="wsu-actions" class="spine-actions clearfix">
 	
 	<ul id="wsu-actions-tabs" class="spine-actions-tabs spine-tabs clearfix">
-		<li id="wsu-search-tab" class="spine-action-tab closed"><button>Search</button></li>
-		<li id="wsu-contact-tab" class="spine-action-tab closed"><button>Contact</button></li>
-		<li id="wsu-share-tab" class="spine-action-tab closed"><button>Share</button></li>
-		<li id="wsu-print-tab" class="spine-action-tab closed"><button>Print</button></li>
+		<li id="wsu-search-tab" class="spine-search-tab closed"><button>Search</button></li>
+		<li id="wsu-contact-tab" class="spine-contact-tab closed"><button>Contact</button></li>
+		<li id="wsu-share-tab" class="spine-share-tab closed"><button>Share</button></li>
+		<li id="wsu-print-tab" class="spine-print-tab closed"><button>Print</button></li>
 	</ul>
 	
 </section><!--/#wsu-actions-->
