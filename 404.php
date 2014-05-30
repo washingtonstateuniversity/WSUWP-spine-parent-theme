cd10e218a0060a1f058aea1eab4adaa9630e81f8
jeremyfelt <jeremy.felt@gmail.com>
2014-05-30 13:58:44 -0700

    Refactor the HTML template for our 404 page

--- 404.php
+++ 404.php
@@ -1,41 +1,28 @@
 <?php get_header(); ?>
 
-<main>
-	
-	<header class="bookmark">
-		<hgroup>
-		    <div class="site"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></div>
-		    <div class="page">Page Not Found</div>
-		</hgroup>
-	</header>
-	
-	<section class="row">
-	
-	<div class="column one">
-	
-	<article id="post-0" class="post error404 no-results not-found">
-		<header class="entry-header">
-			<h1 class="entry-title"><?php _e( 'This is somewhat embarrassing, isn&rsquo;t it?', 'twentytwelve' ); ?></h1>
-		</header>
+	<main class="spine-single-template">
 
-		<div class="entry-content">
-			<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'twentytwelve' ); ?></p>
-			<?php get_search_form(); ?>
-		</div><!-- .entry-content -->
-	
-	<script>
-		/* $(document).ready(function(){
-			$('#wsu-search').clone().appendTo('main');
-		}); */
-	
-	</script>
-	
-	</article>
-	
-	</div>
-	
-	</section>
+			<?php get_template_part('parts/headers'); ?>
 
-</main>
-	
+			<section class="row single">
+
+				<div class="column one">
+
+					<article id="post-0" class="post error404 no-results not-found">
+						<header class="article-header">
+							<h1 class="article-title">This is somewhat embarrassing, isn't it?</h1>
+						</header>
+
+						<div class="entry-content">
+							<p>It seems we can't find what you're looking for. Perhaps searching can help.</p>
+							<?php get_search_form(); ?>
+						</div><!-- .entry-content -->
+
+					</article>
+
+				</div><!--/column-->
+
+			</section>
+	</main>
+
 <?php get_footer(); ?>
