46357182c59a5260eb4973ea70400c688b4e4a35
jeremyfelt <jeremy.felt@gmail.com>
2014-05-30 14:02:26 -0700

    Additional formatting

--- 404.php
+++ 404.php
@@ -2,27 +2,28 @@
 
 	<main class="spine-single-template">
 
-			<?php get_template_part('parts/headers'); ?>
+		<?php get_template_part('parts/headers'); ?>
 
-			<section class="row single">
+		<section class="row single">
 
-				<div class="column one">
+			<div class="column one">
 
-					<article id="post-0" class="post error404 no-results not-found">
-						<header class="article-header">
-							<h1 class="article-title">This is somewhat embarrassing, isn't it?</h1>
-						</header>
+				<article id="post-0" class="post error404 no-results not-found">
 
-						<div class="entry-content">
-							<p>It seems we can't find what you're looking for. Perhaps searching can help.</p>
-							<?php get_search_form(); ?>
-						</div><!-- .entry-content -->
+					<header class="article-header">
+						<h1 class="article-title">This is somewhat embarrassing, isn't it?</h1>
+					</header>
 
-					</article>
+					<div class="entry-content">
+						<p>It seems we can't find what you're looking for. Perhaps searching can help.</p>
+						<?php get_search_form(); ?>
+					</div><!-- .entry-content -->
 
-				</div><!--/column-->
+				</article>
 
-			</section>
+			</div><!--/column-->
+
+		</section>
 	</main>
 
 <?php get_footer(); ?>
