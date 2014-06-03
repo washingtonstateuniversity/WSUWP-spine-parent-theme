211f3beb8ae62a47b57b866e6876c605dd07ba6d
Nathan Jacobson <nate.jacobson@wsu.edu>
2014-06-03 12:51:00 -0700

    Drop .php from template part calls

--- footer.php
+++ footer.php
@@ -1,7 +1,10 @@
 <?php get_template_part( 'spine' ); ?>
 
+<?php get_template_part('parts/binder-after'); ?>
 </div><!--/cover-->
+<?php get_template_part('parts/jacket-after'); ?>
 </div><!--/jacket-->
+<?php get_template_part('parts/body-after'); ?>
 
 <?php get_template_part('parts/contact'); ?> 
 
