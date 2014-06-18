d59271bd4440f45c1882524952b06d7d6bf9fc34
jeremyfelt <jeremy.felt@gmail.com>
2014-06-18 16:10:45 -0700

    Abstract output of section classes input field for admin templates

--- builder-templates/admin/two-columns.php
+++ builder-templates/admin/two-columns.php
@@ -52,11 +52,10 @@ $section_order  = ( ! empty( $ttfmake_section_data['data']['columns-order'] ) ) 
 
 	<div class="clear"></div>
 
-	<div class="wsuwp-builder-meta" style="width:100%; margin-top:10px;">
-		<label for="<?php echo $section_name; ?>[section-classes]">Section Classes</label><input type="text" id="<?php echo $section_name; ?>[section-classes]" class="wsuwp-builder-section-classes widefat" name="<?php echo $section_name; ?>[section-classes]" value="<?php if ( isset( $ttfmake_section_data['data']['section-classes'] ) ) echo esc_attr( $ttfmake_section_data['data']['section-classes'] ); ?>" />
-	</div>
-
-<?php spine_output_builder_section_wrapper( $section_name, $ttfmake_section_data ); ?>
+<?php
+spine_output_builder_section_classes( $section_name, $ttfmake_section_data );
+spine_output_builder_section_wrapper( $section_name, $ttfmake_section_data );
+?>
 
 	<input type="hidden" value="<?php echo esc_attr( implode( ',', $section_order ) ); ?>" name="<?php echo $section_name; ?>[columns-order]" class="wsuwp-spine-halves-columns-order" />
 	<input type="hidden" class="ttfmake-section-state" name="<?php echo $section_name; ?>[state]" value="<?php if ( isset( $ttfmake_section_data['data']['state'] ) ) echo esc_attr( $ttfmake_section_data['data']['state'] ); else echo 'open'; ?>" />
--- inc/builder.php
+++ inc/builder.php
@@ -315,3 +315,17 @@ function spine_output_builder_section_wrapper( $section_name, $ttfmake_section_d
 	</div>
 	<?php
 }
+
+/**
+ * Output the input field for section classes that is shared amongst admin templates.
+ *
+ * @param string $section_name         Current section being displayed.
+ * @param array  $ttfmake_section_data Data associated with the section.
+ */
+function spine_output_builder_section_classes( $section_name, $ttfmake_section_data ) {
+	?>
+	<div class="wsuwp-builder-meta" style="width:100%; margin-top:10px;">
+		<label for="<?php echo $section_name; ?>[section-classes]">Section Classes</label><input type="text" id="<?php echo $section_name; ?>[section-classes]" class="wsuwp-builder-section-classes widefat" name="<?php echo $section_name; ?>[section-classes]" value="<?php if ( isset( $ttfmake_section_data['data']['section-classes'] ) ) echo esc_attr( $ttfmake_section_data['data']['section-classes'] ); ?>" />
+	</div>
+	<?php
+}
