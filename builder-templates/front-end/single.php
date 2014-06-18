fae2b1a4c3bc30af92b8bb589a8a2a90e22573f1
jeremyfelt <jeremy.felt@gmail.com>
2014-06-17 17:02:34 -0700

    Output entered section classes as part of section element

--- builder-templates/front-end/single.php
+++ builder-templates/front-end/single.php
@@ -1,7 +1,14 @@
 <?php
 global $ttfmake_section_data, $ttfmake_sections;
+
+if ( isset( $ttfmake_section_data['section-classes'] ) ) {
+	$section_classes = $ttfmake_section_data['section-classes'];
+} else {
+	$section_classes = '';
+}
+
 ?>
-<section id="builder-section-<?php echo esc_attr( $ttfmake_section_data['id'] ); ?>" class="row single">
+<section id="builder-section-<?php echo esc_attr( $ttfmake_section_data['id'] ); ?>" class="row single <?php echo $section_classes; ?>">
 	<div class="column one">
 
 		<?php if ( ! empty( $ttfmake_section_data['title'] ) ) : ?>
