ac70c1741664f063876dcc517fa630f3c4e9861a
jeremyfelt <jeremy.felt@gmail.com>
2014-06-18 09:44:41 -0700

    Refactor admin/single for readability

--- builder-templates/admin/single.php
+++ builder-templates/admin/single.php
@@ -1,28 +1,32 @@
-<?php ttfmake_load_section_header(); ?>
-<?php global $ttfmake_section_data, $ttfmake_is_js_template; ?>
-<?php $section_name = ttfmake_get_section_name( $ttfmake_section_data, $ttfmake_is_js_template ); ?>
+<?php
+global $ttfmake_section_data, $ttfmake_is_js_template;
 
+$section_name = ttfmake_get_section_name( $ttfmake_section_data, $ttfmake_is_js_template );
+
+ttfmake_load_section_header();
+?>
 	<div class="ttfmake-titlediv">
 		<div class="ttfmake-titlewrap">
 			<input placeholder="<?php esc_attr_e( 'Enter title here' ); ?>" type="text" name="<?php echo $section_name; ?>[title]" class="ttfmake-title ttfmake-section-header-title-input" value="<?php if ( isset( $ttfmake_section_data['data']['title'] ) ) echo esc_attr( htmlspecialchars( $ttfmake_section_data['data']['title'] ) ); ?>" autocomplete="off" />
 		</div>
 	</div>
-
 <?php
+
 $editor_settings = array(
 	'tinymce'       => true,
 	'quicktags'     => true,
 	'editor_height' => 345,
-	'textarea_name' => $section_name . '[content]'
+	'textarea_name' => $section_name . '[content]',
 );
 
-if ( true === $ttfmake_is_js_template ) : ?>
-	<?php ttfmake_get_builder_base()->wp_editor( '', ttfmake_get_wp_editor_id( $ttfmake_section_data, $ttfmake_is_js_template ), $editor_settings ); ?>
-<?php else : ?>
-	<?php $content = ( isset( $ttfmake_section_data['data']['content'] ) ) ? $ttfmake_section_data['data']['content'] : ''; ?>
-	<?php ttfmake_get_builder_base()->wp_editor( $content, ttfmake_get_wp_editor_id( $ttfmake_section_data, $ttfmake_is_js_template ), $editor_settings ); ?>
-<?php endif; ?>
+if ( true === $ttfmake_is_js_template ) {
+	ttfmake_get_builder_base()->wp_editor( '', ttfmake_get_wp_editor_id( $ttfmake_section_data, $ttfmake_is_js_template ), $editor_settings );
+} else {
+	$content = ( isset( $ttfmake_section_data['data']['content'] ) ) ? $ttfmake_section_data['data']['content'] : '';
+	ttfmake_get_builder_base()->wp_editor( $content, ttfmake_get_wp_editor_id( $ttfmake_section_data, $ttfmake_is_js_template ), $editor_settings );
+}
 
+?>
 	<div class="wsuwp-builder-meta" style="width:100%; margin-top:10px;">
 		<label for="<?php echo $section_name; ?>[section-classes]">Section Classes</label><input type="text" id="<?php echo $section_name; ?>[section-classes]" class="wsuwp-builder-section-classes widefat" name="<?php echo $section_name; ?>[section-classes]" value="<?php if ( isset( $ttfmake_section_data['data']['section-classes'] ) ) echo esc_attr( $ttfmake_section_data['data']['section-classes'] ); ?>" />
 	</div>
