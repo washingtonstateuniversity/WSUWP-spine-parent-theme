fc4c28db892f890fffb699619236b37e2e82bd8a
jeremyfelt <jeremy.felt@gmail.com>
2014-06-18 09:46:55 -0700

    Reduce indent on HTML output in admin/single template

--- builder-templates/admin/single.php
+++ builder-templates/admin/single.php
@@ -5,11 +5,13 @@ $section_name = ttfmake_get_section_name( $ttfmake_section_data, $ttfmake_is_js_
 
 ttfmake_load_section_header();
 ?>
-	<div class="ttfmake-titlediv">
-		<div class="ttfmake-titlewrap">
-			<input placeholder="<?php esc_attr_e( 'Enter title here' ); ?>" type="text" name="<?php echo $section_name; ?>[title]" class="ttfmake-title ttfmake-section-header-title-input" value="<?php if ( isset( $ttfmake_section_data['data']['title'] ) ) echo esc_attr( htmlspecialchars( $ttfmake_section_data['data']['title'] ) ); ?>" autocomplete="off" />
-		</div>
+
+<div class="ttfmake-titlediv">
+	<div class="ttfmake-titlewrap">
+		<input placeholder="<?php esc_attr_e( 'Enter title here' ); ?>" type="text" name="<?php echo $section_name; ?>[title]" class="ttfmake-title ttfmake-section-header-title-input" value="<?php if ( isset( $ttfmake_section_data['data']['title'] ) ) echo esc_attr( htmlspecialchars( $ttfmake_section_data['data']['title'] ) ); ?>" autocomplete="off" />
 	</div>
+</div>
+
 <?php
 
 $editor_settings = array(
@@ -27,9 +29,10 @@ if ( true === $ttfmake_is_js_template ) {
 }
 
 ?>
-	<div class="wsuwp-builder-meta" style="width:100%; margin-top:10px;">
-		<label for="<?php echo $section_name; ?>[section-classes]">Section Classes</label><input type="text" id="<?php echo $section_name; ?>[section-classes]" class="wsuwp-builder-section-classes widefat" name="<?php echo $section_name; ?>[section-classes]" value="<?php if ( isset( $ttfmake_section_data['data']['section-classes'] ) ) echo esc_attr( $ttfmake_section_data['data']['section-classes'] ); ?>" />
-	</div>
-	<input type="hidden" class="ttfmake-section-state" name="<?php echo $section_name; ?>[state]" value="<?php if ( isset( $ttfmake_section_data['data']['state'] ) ) echo esc_attr( $ttfmake_section_data['data']['state'] ); else echo 'open'; ?>" />
+
+<div class="wsuwp-builder-meta" style="width:100%; margin-top:10px;">
+	<label for="<?php echo $section_name; ?>[section-classes]">Section Classes</label><input type="text" id="<?php echo $section_name; ?>[section-classes]" class="wsuwp-builder-section-classes widefat" name="<?php echo $section_name; ?>[section-classes]" value="<?php if ( isset( $ttfmake_section_data['data']['section-classes'] ) ) echo esc_attr( $ttfmake_section_data['data']['section-classes'] ); ?>" />
+</div>
+<input type="hidden" class="ttfmake-section-state" name="<?php echo $section_name; ?>[state]" value="<?php if ( isset( $ttfmake_section_data['data']['state'] ) ) echo esc_attr( $ttfmake_section_data['data']['state'] ); else echo 'open'; ?>" />
 
 <?php ttfmake_load_section_footer(); ?>
