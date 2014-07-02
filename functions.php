f7f46e240f2179fe702e5a3828479d0600a07858
jeremyfelt <jeremy.felt@gmail.com>
2014-07-02 11:36:02 -0700

    Remove welcome.php and replace with filter of default content
    WSUWP allows us to override the default content for the home page
    rather than including a template

--- functions.php
+++ functions.php
@@ -419,3 +419,29 @@ function spine_sectioned_body_classes( $classes ) {
 
 	return array_unique( $classes );
 }
+
+add_filter( 'wsuwp_first_page_content', 'spine_install_default_content' );
+/**
+ * Filter the content used for the home page when creating a new site on the WSUWP Platform.
+ *
+ * @return string Default content to add to the home page.
+ */
+function spine_install_default_content() {
+	$page_content = '<article>
+	<h2>Welcome to the WSU Web</h2>
+	<p>As a visual element, the WSU Spine is a 198px wide column that binds together the many websites of wsu.edu. As a framework, the WSU Spine is a minimal template that provides global tools and a responsive and flexible grid for every WSU website. With a uniform and global spine on the left and a blank, unwritten page to the right, the Spine balances the unity and diversity of our university.</p>
+	<img src="' . esc_url( get_template_directory_uri() . '/admin/customizer.png' ) . '" class="alignright">
+	<h2>Getting Started</h2>
+	<ol>
+		<li>After <a href="' . esc_url( wp_login_url() ) . '">logging in</a>, head to the <a href="' . esc_url( admin_url( 'customize.php?theme=spine' ) ) . '">Customizer</a>.</li>
+		<li>Enter your Site Title and Tagline.</li>
+		<li>Expand "Contact Details" and enter the information of the unit responsible for this site.</li>
+		<li>Optionally, you can replace or remove one or more of the university\'s social channels.</li>
+		<li>Optionally, you can alter the Spine\'s default behavior in "Spine Options".</li>
+		<li>Head to <a href="' . esc_url( admin_url( 'edit.php?post_type=page' ) ) . '">Pages</a> and <a href="' . esc_url( admin_url( 'nav-menus.php' ) ) . '">Appearance -> Menus</a> to begin building out your site.</li>
+		<li>And finally, delete or modify your <a href="' . esc_url( admin_url( 'post.php?post=1&action=edit' ) ) . '">Hello World post</a> to remove this primer.</li>
+	</ol>
+</article>';
+
+	return $page_content;
+}
