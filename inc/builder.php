d6f2bdb5c1d08a9d072f46bb4ec14235834961a7
Nathan Jacobson <nate.jacobson@wsu.edu>
2014-07-03 09:30:30 -0700

    Remove conditional section classes

--- inc/builder.php
+++ inc/builder.php
@@ -344,12 +344,8 @@ function spine_output_builder_section_wrapper( $section_name, $ttfmake_section_d
  * @param array  $ttfmake_section_data Data associated with the section.
  */
 function spine_output_builder_section_classes( $section_name, $ttfmake_section_data ) {
-	//if ( !isset( $ttfmake_section_data['data']['section-classes'] ) && $ttfmake_section_data['section']['id'] == 'wsuwpheader' ) {
-	//	$section_classes = 'gutter';
-	//} else {
-		$section_classes = ( isset( $ttfmake_section_data['data']['section-classes'] ) ) ? $ttfmake_section_data['data']['section-classes'] : 'gutter pad-top';
-	//}
-	?><?php print_r($ttfmake_section_data); ?>
+	$section_classes = ( isset( $ttfmake_section_data['data']['section-classes'] ) ) ? $ttfmake_section_data['data']['section-classes'] : 'gutter pad-top';
+	?>
 	<div class="wsuwp-builder-meta" style="width:100%; margin-top:10px;">
 		<label for="<?php echo $section_name; ?>[section-classes]">Section Classes</label><input type="text" id="<?php echo $section_name; ?>[section-classes]" class="wsuwp-builder-section-classes widefat" name="<?php echo $section_name; ?>[section-classes]" value="<?php echo esc_attr( $section_classes ); ?>" />
 		<p class="description">Enter space delimited class names here to apply them to the <code>section</code> element represented by this builder area.</p>
