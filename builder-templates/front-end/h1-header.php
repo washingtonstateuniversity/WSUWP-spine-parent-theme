ed45533947e4f3b90ed7e4c5e34a7fe3d673d270
Nathan Jacobson <nate.jacobson@wsu.edu>
2014-07-21 16:31:00 -0700

    Adjust spacing when h1-header is employed

--- builder-templates/front-end/h1-header.php
+++ builder-templates/front-end/h1-header.php
@@ -8,7 +8,7 @@ if ( $section_wrapper_classes ) {
 	echo '<div class="' . esc_attr( $section_wrapper_classes ) . '">';
 }
 ?>
-<section id="builder-section-<?php echo esc_attr( $ttfmake_section_data['id'] ); ?>" class="row single <?php echo esc_attr( $section_classes ); ?>">
+<section id="builder-section-<?php echo esc_attr( $ttfmake_section_data['id'] ); ?>" class="row single h1-header <?php echo esc_attr( $section_classes ); ?>">
 	<div class="column one">
 		<?php if ( ! empty( $ttfmake_section_data['title'] ) ) : ?>
 			<h1><?php echo apply_filters( 'the_title', $ttfmake_section_data['title'] ); ?></h1>
