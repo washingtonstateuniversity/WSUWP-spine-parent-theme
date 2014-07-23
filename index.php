442a8a0cc14c52247711513de75297aa5df88320
jeremyfelt <jeremy.felt@gmail.com>
2014-07-23 12:35:40 -0700

    Provide pagination on all archive pages

--- index.php
+++ index.php
@@ -41,7 +41,21 @@ if ( is_home() ) {
 	</div><!--/column two-->
 
 </section>
+<?php
+/* @type WP_Query $wp_query */
+global $wp_query;
 
-</main><!--/#page-->
+$big = 99164;
+$args = array(
+	'base'         => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+	'format'       => 'page/%#%',
+	'total'        => $wp_query->max_num_pages, // Provide the number of pages this query expects to fill.
+	'current'      => max( 1, get_query_var('paged') ), // Provide either 1 or the page number we're on.
+);
+echo paginate_links( $args );
 
-<?php get_footer(); ?>
+?>
+</main>
+<?php
+
+get_footer();
