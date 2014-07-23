69a5e3913d72e31a27c02c87e6d8e3f14941bae1
jeremyfelt <jeremy.felt@gmail.com>
2014-07-23 12:33:01 -0700

    Use index.php for all default archives, differentiate with class
    Rather than maintaining identical templates through tag.php,
    category.php, archive.php, and index.php, we can use index.php
    to serve all archive requests.
    
    If at any point we decide differen HTML markup is necessary for
    a specific view, we can then break these out.

--- index.php
+++ index.php
@@ -1,6 +1,24 @@
-<?php get_header(); ?>
+<?php
 
-<main class="spine-main-index">
+get_header();
+
+if ( is_home() ) {
+	$main_class = 'spine-main-index';
+} elseif ( is_author() ) {
+	$main_class = 'spine-author-index';
+} elseif ( is_category() ) {
+	$main_class = 'spine-category-index';
+} elseif ( is_tag() ) {
+	$main_class = 'spine-tag-index';
+} elseif ( is_tax() ) {
+	$main_class = 'spine-tax-index';
+} elseif ( is_archive() ) {
+	$main_class = 'spine-archive-index';
+}
+
+?>
+
+<main class="<?php echo $main_class; ?>">
 
 <?php get_template_part('parts/headers'); ?> 
 
