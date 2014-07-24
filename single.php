85479f3b9a1c14a4a39b1516ac83218514447316
Nathan Jacobson <nate.jacobson@wsu.edu>
2014-07-24 15:48:01 -0700

    Add featured and background images

--- single.php
+++ single.php
@@ -3,23 +3,35 @@
 get_header();
 
 // If a featured image is assigned to the post, display as a background image.
-if ( spine_has_featured_image() ) {
-	$image = spine_get_featured_image_src( 'single-post-thumbnail' );
-	?><style> main { background-image: url(<?php echo esc_url( $image ); ?>); }</style><?php
+if ( spine_has_background_image() ) {
+	$background_image_src = spine_get_background_image_src();
+	?><style> html { background-image: url(<?php echo esc_url( $background_image_src ); ?>); }</style><?php
 }
 
 // If a position has been assigned to the featured image as a background, apply that style.
-$position = get_post_meta( get_the_id(), 'position', true );
+/* $position = get_post_meta( get_the_id(), 'position', true );
 if ( ! empty( $position ) ) {
 	$position = absint( $position ) - 132;
 	?><style>main section:nth-of-type(1) { margin-top: <?php echo $position; ?>px; }</style><?php
-}
+} */
 
 ?>
 
 <main>
 
 <?php get_template_part('parts/headers'); ?>
+
+<?php if ( spine_has_featured_image() ) { 
+$featured_image_src = spine_get_featured_image_src(); ?>
+<figure class="featured-image" style="background-image: url('<?php echo $featured_image_src ?>');">
+	<?php spine_the_featured_image(); ?>
+</figure>
+<?php } ?>
+
+<style>
+
+
+</style>
 
 <section class="row sidebar side-right gutter marginalize-ends">
 
