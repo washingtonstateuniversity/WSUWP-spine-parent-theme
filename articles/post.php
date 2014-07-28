4be6657a14ecc243e8e3ad601c2fa62ff7d83e5e
jeremyfelt <jeremy.felt@gmail.com>
2014-07-28 11:49:28 -0700

    Adjust spacing for readability

--- articles/post.php
+++ articles/post.php
@@ -1,12 +1,12 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	
+
 	<header class="article-header">
 		<hgroup>
 		<?php if ( is_single() ) : ?>
 			<?php if ( spine_get_option( 'articletitle_show' ) == 'true' ) : ?>
 				<h1 class="article-title"><?php the_title(); ?></h1>
 			<?php endif; ?>
-			<?php else : ?>
+		<?php else : ?>
 			<h2 class="article-title">
 				<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
 			</h2>
@@ -17,61 +17,48 @@
 			<cite class="article-author" role="author"><?php the_author_posts_link(); ?></cite>
 		</hgroup>
 	</header>
-	
+
 	<?php if ( !is_singular() ) : ?>
-	
 		<div class="article-summary">
-		
-			<?php if ( has_post_thumbnail()) : ?>
-			
-				<figure class="article-thumbnail"><?php the_post_thumbnail(array(132,132,true)); ?></figure>
-				
-			<?php endif; ?>
-			
 			<?php
-			
-				if ( $post->post_excerpt ) {
-				
-					echo get_the_excerpt();
-					echo '<a href="'.get_permalink().'"><span class="excerpt-more-default">&raquo; More ...</span></a>';
-					
-					
-				} elseif (strstr($post->post_content,'<!--more-->')) {
-				
-					the_content('<span class="content-more-default">&raquo; More ...</span>');
-					
-				} else {
-				
-					the_excerpt();
-					
-				} ?>
-							
+
+			if ( has_post_thumbnail()) {
+				?><figure class="article-thumbnail"><?php the_post_thumbnail(array(132,132,true)); ?></figure><?php
+			}
+
+			if ( $post->post_excerpt ) {
+				echo get_the_excerpt();
+				echo '<a href="' . get_permalink() . '"><span class="excerpt-more-default">&raquo; More ...</span></a>';
+			} elseif ( strstr( $post->post_content, '<!--more-->' ) ) {
+				the_content( '<span class="content-more-default">&raquo; More ...</span>' );
+			} else {
+				the_excerpt();
+			}
+			?>
 		</div><!-- .article-summary -->
-	
 	<?php else : ?>
 		<div class="article-body">
 			<?php the_content(); ?>
 			<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'spine' ), 'after' => '</div>' ) ); ?>
 		</div>
-		<?php endif; ?>
-		
+	<?php endif; ?>
+
 	<?php if ( comments_open() && is_singular() ) : ?>
 	<blockquote class="comments">
-	
+
 	</blockquote>
 	<?php endif; // comments_open() ?>
-		
+
 	<footer class="article-footer">
-			
+
 			<?php
-				
 				// Categorized In...
 				if ( has_category()) {
 					echo '<dl class="categorized">';
 					echo '<dt><span class="categorized-default">Categorized</span></dt>';
 					foreach((get_the_category()) as $category) {
-					    echo '<dd><a href="'.get_category_link($category->cat_ID).'">' . $category->cat_name . '</a></dd>';
-					    }    
+						echo '<dd><a href="'.get_category_link($category->cat_ID).'">' . $category->cat_name . '</a></dd>';
+					}
 					echo '</dl>';
 				}
 				// Tagged As...
@@ -79,14 +66,13 @@
 					echo '<dl class="tagged">';
 					echo '<dt><span class="tagged-default">Tagged</span></dt>';
 					foreach((get_the_tags()) as $tag) {
-					    echo '<dd><a href="'.get_tag_link($tag->term_id).'">' . $tag->name . '</a></dd>';
-					    }    
+						echo '<dd><a href="'.get_tag_link($tag->term_id).'">' . $tag->name . '</a></dd>';
+					}
 					echo '</dl>';
 				}
 				// Comments Allowed
-				if ( comments_open()) {
-				
-				}
+				// if ( comments_open()) {}
+
 				// User Can Edit
 				if ( current_user_can('edit_post', $post->ID) && !is_singular() ) {
 					echo '<dl class="editors">';
