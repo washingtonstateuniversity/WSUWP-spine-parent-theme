0ffdf168f2e5e6caadec515dd389e276a16b33d4
Nathan Jacobson <nate.jacobson@wsu.edu>
2014-07-31 15:42:31 -0700

    Put featured images code into single include file

---
+++ parts/featured-images.php
@@ -0,0 +1,18 @@
+<?php
+
+// If a featured image is assigned to the post, display as a background image.
+
+if ( spine_has_background_image() ) {
+	$background_image_src = spine_get_background_image_src();
+	?>
+	
+	<style> html { background-image: url(<?php echo esc_url( $background_image_src ); ?>); }</style>
+	
+<?php } ?>
+
+<?php if ( spine_has_featured_image() ) { 
+$featured_image_src = spine_get_featured_image_src(); ?>
+<figure class="featured-image" style="background-image: url('<?php echo $featured_image_src ?>');">
+	<?php spine_the_featured_image(); ?>
+</figure>
+<?php } ?>
