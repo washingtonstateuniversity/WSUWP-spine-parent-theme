e97b9803ea421649c55de26ebcf0c336ea94d9f3
jeremyfelt <jeremy.felt@gmail.com>
2014-08-08 15:47:36 -0700

    Add support for HTML5 image galleries
    This will stop WordPress from outputting default styles targetted
    at the gallery in the DOM. These styles required the use of important
    to override.

--- includes/theme-setup.php
+++ includes/theme-setup.php
@@ -3,8 +3,18 @@
  * Class Spine_Theme_Setup
  */
 class Spine_Theme_Setup {
+	/**
+	 * Setup hooks.
+	 */
 	public function __construct() {
+		add_action( 'after_setup_theme', array( $this, 'add_theme_support' ) );
+	}
 
+	/**
+	 * Add theme support for various features provided by WordPress.
+	 */
+	public function add_theme_support() {
+		add_theme_support( 'html5', array( 'gallery' ) );
 	}
 }
 new Spine_Theme_Setup();
