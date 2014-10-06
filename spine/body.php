9294b113b7b19698b94708bcc577de065ea0e944
jeremyfelt <jeremy.felt@gmail.com>
2014-10-06 16:30:51 -0700

    Move Spine navigation section to template part
    This will allow child themes to take over the generation of the
    nav menu in the spine without having to also maintain the elements
    of the body

--- spine/body.php
+++ spine/body.php
@@ -10,49 +10,7 @@
 
 <?php get_template_part('spine/header'); ?>
 
-<section id="spine-navigation" class="spine-navigation">
-
-	<nav id="spine-sitenav" class="spine-sitenav">
-	<?php
-	$site = array(
-		'theme_location'  => 'site',
-		'menu'            => 'site',
-		'container'       => false,
-		'container_class' => false,
-		'container_id'    => false,
-		'menu_class'      => null,
-		'menu_id'         => null,
-		'echo'            => true,
-		'fallback_cb'     => 'wp_page_menu',
-		'items_wrap'      => '<ul>%3$s</ul>',
-		'depth'           => 5,
-		'walker'          => ''
-		);
-	wp_nav_menu( $site );
-	?>
-	</nav>
-	
-	<nav id="spine-offsitenav" class="spine-offsitenav">
-	<?php 
-	$offsite = array(
-		'theme_location'  => 'offsite',
-		'menu'            => 'offsite',
-		'container'       => false,
-		'container_class' => false,
-		'container_id'    => false,
-		'menu_class'      => null,
-		'menu_id'         => null,
-		'echo'            => true,
-		'fallback_cb'     => false,
-		'items_wrap'      => '<ul>%3$s</ul>',
-		'depth'           => 3,
-		'walker'          => ''
-	);
-	wp_nav_menu( $offsite );
-	?>
-	</nav>
-	
-</section>
+<?php get_template_part( 'spine/navigation' ); ?>
 		
 <?php get_template_part('spine/footer'); ?>
 
---
+++ spine/navigation.php
@@ -0,0 +1,43 @@
+<section id="spine-navigation" class="spine-navigation">
+
+	<nav id="spine-sitenav" class="spine-sitenav">
+		<?php
+		$site = array(
+			'theme_location'  => 'site',
+			'menu'            => 'site',
+			'container'       => false,
+			'container_class' => false,
+			'container_id'    => false,
+			'menu_class'      => null,
+			'menu_id'         => null,
+			'echo'            => true,
+			'fallback_cb'     => 'wp_page_menu',
+			'items_wrap'      => '<ul>%3$s</ul>',
+			'depth'           => 5,
+			'walker'          => ''
+		);
+		wp_nav_menu( $site );
+		?>
+	</nav>
+
+	<nav id="spine-offsitenav" class="spine-offsitenav">
+		<?php
+		$offsite = array(
+			'theme_location'  => 'offsite',
+			'menu'            => 'offsite',
+			'container'       => false,
+			'container_class' => false,
+			'container_id'    => false,
+			'menu_class'      => null,
+			'menu_id'         => null,
+			'echo'            => true,
+			'fallback_cb'     => false,
+			'items_wrap'      => '<ul>%3$s</ul>',
+			'depth'           => 3,
+			'walker'          => ''
+		);
+		wp_nav_menu( $offsite );
+		?>
+	</nav>
+
+</section>
