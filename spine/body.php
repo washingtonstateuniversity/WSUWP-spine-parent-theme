572670b37bdc13e9885dd715c786b23abec71eae
jeremyfelt <jeremy.felt@gmail.com>
2014-10-06 16:33:47 -0700

    Code formatting

--- spine/body.php
+++ spine/body.php
@@ -1,18 +1,21 @@
-<?php if ( true == spine_get_option( 'crop' ) && is_front_page() ) {
-		$cropping = ' cropped';
-	} else {
-		$cropping = '';
-	}
+<?php
+
+if ( true == spine_get_option( 'crop' ) && is_front_page() ) {
+	$cropping = ' cropped';
+} else {
+	$cropping = '';
+}
+
 ?>
 
 <div id="spine" class="spine-column <?php echo esc_attr( spine_get_option( 'spine_color' ) ); echo $cropping; echo esc_attr( spine_get_option( 'bleed' ) ); ?> shelved">
 <div id="glue" class="spine-glue">
 
-<?php get_template_part('spine/header'); ?>
+<?php get_template_part( 'spine/header' ); ?>
 
 <?php get_template_part( 'spine/navigation' ); ?>
 		
-<?php get_template_part('spine/footer'); ?>
+<?php get_template_part( 'spine/footer' ); ?>
 
 </div><!--/glue-->
 </div><!--/spine-->
