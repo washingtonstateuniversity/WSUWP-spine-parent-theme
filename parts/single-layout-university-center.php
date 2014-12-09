abfb92d17de377494ec5ec8f833afb7291270801
jeremyfelt <jeremy.felt@gmail.com>
2014-12-08 17:01:07 -0800

    Individual people, projects, entities, publications should be single layout

--- parts/single-layout-university-center.php
+++ parts/single-layout-university-center.php
@@ -1,4 +1,4 @@
-<section class="row side-right gutter pad-ends">
+<section class="row single gutter pad-ends">
 
 	<div class="column one">
 
@@ -11,7 +11,5 @@
 		<?php endwhile; ?>
 
 	</div><!--/column-->
-
-	<div class="column two"></div><!--/column two-->
 
 </section>
