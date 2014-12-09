f10116c7b2c28c3a6c0a70011ab6a30b1f558f6b
jeremyfelt <jeremy.felt@gmail.com>
2014-12-08 16:36:12 -0800

    Remove other unused parts of a person layout

--- articles/post-wsuwp_uc_person.php
+++ articles/post-wsuwp_uc_person.php
@@ -18,19 +18,10 @@
 				?><figure class="article-thumbnail"><?php the_post_thumbnail( array( 132, 132, true ) ); ?></figure><?php
 			}
 
-			// If a manual excerpt is available, default to that. If `<!--more-->` exists in content, default
-			// to that. If an option is set specifically to display excerpts, default to that. Otherwise show
-			// full content.
+			// If a manual excerpt is available, display this. Otherwise, only the most basic information is needed.
 			if ( $post->post_excerpt ) {
-				echo get_the_excerpt() . ' <a href="' . get_permalink() . '"><span class="excerpt-more-default">&raquo; More ...</span></a>';
-			} elseif ( strstr( $post->post_content, '<!--more-->' ) ) {
-				the_content( '<span class="content-more-default">&raquo; More ...</span>' );
-			} elseif ( 'excerpt' === spine_get_option( 'archive_content_display' ) ) {
-				the_excerpt();
-			} else {
-				the_content();
+				echo get_the_excerpt();
 			}
-
 			?>
 		</div><!-- .article-summary -->
 	<?php else : ?>
@@ -39,12 +30,6 @@
 			<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'spine' ), 'after' => '</div>' ) ); ?>
 		</div>
 	<?php endif; ?>
-
-	<?php if ( comments_open() && is_singular() ) : ?>
-		<blockquote class="comments">
-
-		</blockquote>
-	<?php endif; // comments_open() ?>
 
 	<footer class="article-footer">
 		<?php
@@ -101,36 +86,7 @@
 				echo '</dl>';
 			}
 		}
-
-		// Comments Allowed
-		// if ( comments_open()) {}
-
-		// If the user viewing the post can edit it, show an edit link.
-		if ( current_user_can( 'edit_post', $post->ID ) && ! is_singular() ) {
-			?><dl class="editors"><?php edit_post_link( 'Edit', '<span class="edit-link">', '</span>' ); ?></dl><?php
-		}
-
-		// If a user has filled out their description and this is a multi-author blog, show a bio on their entries.
-		if ( is_singular() && get_the_author_meta( 'description' ) && is_multi_author() ) : ?>
-			<div class="author-info">
-				<div class="author-avatar">
-					<?php
-					/** This filter is documented in author.php */
-					$author_bio_avatar_size = apply_filters( 'twentytwelve_author_bio_avatar_size', 68 );
-					echo get_avatar( get_the_author_meta( 'user_email' ), $author_bio_avatar_size );
-					?>
-				</div><!-- .author-avatar -->
-				<div class="author-description">
-					<h2><?php printf( __( 'About %s', 'twentytwelve' ), get_the_author() ); ?></h2>
-					<p><?php the_author_meta( 'description' ); ?></p>
-					<div class="author-link">
-						<a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author">
-							<?php printf( __( 'View all posts by %s <span class="meta-nav">&rarr;</span>', 'twentytwelve' ), get_the_author() ); ?>
-						</a>
-					</div><!-- .author-link	-->
-				</div><!-- .author-description -->
-			</div><!-- .author-info -->
-		<?php endif; ?>
+		?>
 	</footer><!-- .entry-meta -->
 
 </article>
