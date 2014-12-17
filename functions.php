1221df25090d2e7a7d443a90af16261e793c2086
jeremyfelt <jeremy.felt@gmail.com>
2014-12-17 11:15:57 -0800

    Bump version 0.17.0

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 // Global version tracker.
-$wsuwp_spine_theme_version = '0.16.3';
+$wsuwp_spine_theme_version = '0.17.0';
 
 include_once( 'includes/theme-setup.php' ); // Setup basic portions of the theme.
 include_once( 'includes/main-header.php' ); // Include main header functionality.
