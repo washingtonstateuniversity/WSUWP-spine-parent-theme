6368fed8bfbb027f05548e8b6e2fc07eeb96c8de
jeremyfelt <jeremy.felt@gmail.com>
2015-02-05 13:54:55 -0800

    Introduce a class to use for all Spine navigation customization

---
+++ includes/theme-navigation.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Class Spine_Theme_Navigation
+ *
+ * Manages the various customizations to navigation used by the Spine theme. This
+ * includes adjustments based on other plugins in addition to WordPress core.
+ */
+class Spine_Theme_Navigation {
+	public function __construct() {
+		
+	}
+}
+new Spine_Theme_Navigation();
