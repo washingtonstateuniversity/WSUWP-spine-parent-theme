5b83bca98a7c1b6c8fa66ab70f7f1642c07b5610
jeremyfelt <jeremy.felt@gmail.com>
2015-03-19 14:48:58 -0700

    Provide a background and background mobile image input on sections
    Specifying background images at the section level allows us to be
    more flexible with how these background images are used on output.
    
    This is an in progress feature that should not be relied on yet,
    but will be built out soon.

--- builder-templates/admin/banner.php
+++ builder-templates/admin/banner.php
@@ -117,6 +117,7 @@ $section_order = ( ! empty( $ttfmake_section_data['data']['banner-slide-order'] 
 				spine_output_builder_section_wrapper( $section_name, $ttfmake_section_data );
 				spine_output_builder_section_label( $section_name, $ttfmake_section_data );
 				spine_output_builder_column_classes( $section_name, $ttfmake_section_data );
+				spine_output_builder_section_background( $section_name, $ttfmake_section_data );
 				?>
 			</div>
 		</div>
