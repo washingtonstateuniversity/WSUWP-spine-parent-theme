95cb2c5bbf635a17358f1275b23cd3a0e2d7255c
jeremyfelt <jeremy.felt@gmail.com>
2015-03-20 10:48:46 -0700

    Apply wrapping backgroud to banner and headline sections

--- builder-templates/front-end/banner.php
+++ builder-templates/front-end/banner.php
@@ -40,8 +40,33 @@ $slider_ratio = ( $slider_height / 960 ) * 100;
 	<?php endif; ?>
 </style>
 <?php
+if ( isset( $ttfmake_section_data['background-img'] ) && ! empty( $ttfmake_section_data['background-img'] ) ) {
+	$section_background = $ttfmake_section_data['background-img'];
+} else {
+	$section_background = false;
+}
+
+if ( isset( $ttfmake_section_data['background-mobile-img'] ) && ! empty( $ttfmake_section_data['background-mobile-img'] ) ) {
+	$section_mobile_background = $ttfmake_section_data['background-mobile-img'];
+} elseif( $section_background ) {
+	$section_mobile_background = $section_background;
+} else {
+	$section_mobile_background = false;
+}
+
+if ( $section_background || $section_mobile_background ) {
+	if ( $section_wrapper_classes ) {
+		$section_wrapper_classes .= ' section-wrapper-has-background';
+	} else {
+		$section_wrapper_classes = 'section-wrapper-has-background';
+	}
+}
+
 if ( $section_wrapper_classes ) {
-	echo '<div class="' . esc_attr( $section_wrapper_classes ) . '">';
+	?><div class="<?php echo esc_attr( $section_wrapper_classes ); ?>"
+	<?php if ( $section_background ) : echo 'data-background="' . esc_url( $section_background ) . '"'; endif; ?>
+	<?php if ( $section_mobile_background ) : echo 'data-background-mobile="' . esc_url( $section_mobile_background ) . '"'; endif; ?>>
+<?php
 }
 ?>
 <section id="builder-section-<?php echo esc_attr( $ttfmake_section_data['id'] ); ?>" class="row single builder-section <?php echo $section_classes; ?> <?php echo esc_attr( ttfmake_builder_get_banner_class( $ttfmake_section_data, $ttfmake_sections ) ); ?>">
