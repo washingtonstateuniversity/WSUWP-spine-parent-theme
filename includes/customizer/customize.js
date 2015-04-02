ec892892a4f83e8b2f930dacb6674182d32e7b01
jeremyfelt <jeremy.felt@gmail.com>
2015-04-02 09:56:55 -0700

    Wait until document ready before using jQuery to add placeholders

--- includes/customizer/customize.js
+++ includes/customizer/customize.js
@@ -1,9 +1,13 @@
 (function($){
-	$('.accordion-section label input').focusin( function() { $(this).closest('label').addClass('focused'); } );
-	$('.accordion-section label input').focusout( function() { $(this).closest('label').removeClass('focused'); } );
+	$(document).ready(function(){
+		var $accordion_section = $('.accordion-section label input');
 
-	$('#customize-control-social_spot_one input').attr('placeholder','http://www.facebook.com/wsupullman');
-	$('#customize-control-social_spot_two input').attr('placeholder','http://twitter.com/wsupullman');
-	$('#customize-control-social_spot_three input').attr('placeholder','http://youtube.com/washingtonstateuniv');
-	$('#customize-control-social_spot_four input').attr('placeholder','http://social.wsu.edu');
+		$accordion_section.focusin( function() { $(this).closest('label').addClass('focused'); } );
+		$accordion_section.focusout( function() { $(this).closest('label').removeClass('focused'); } );
+
+		$('#customize-control-social_spot_one').find('input').attr('placeholder','http://www.facebook.com/wsupullman');
+		$('#customize-control-social_spot_two').find('input').attr('placeholder','http://twitter.com/wsupullman');
+		$('#customize-control-social_spot_three').find('input').attr('placeholder','http://youtube.com/washingtonstateuniv');
+		$('#customize-control-social_spot_four').find('input').attr('placeholder','http://social.wsu.edu');
+	});
 }(jQuery));
