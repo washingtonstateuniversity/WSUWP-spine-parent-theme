a0f7a307e91307aaa84485b12e3d30c2c9480e06
jeremyfelt <jeremy.felt@gmail.com>
2015-04-02 09:58:07 -0700

    Use HTTPS URLs in the placeholders for social spots

--- includes/customizer/customize.js
+++ includes/customizer/customize.js
@@ -1,8 +1,8 @@
 (function($){
 	$(document).ready(function(){
-		$('#customize-control-social_spot_one').find('input').attr('placeholder','http://www.facebook.com/wsupullman');
-		$('#customize-control-social_spot_two').find('input').attr('placeholder','http://twitter.com/wsupullman');
-		$('#customize-control-social_spot_three').find('input').attr('placeholder','http://youtube.com/washingtonstateuniv');
+		$('#customize-control-social_spot_one').find('input').attr('placeholder','https://www.facebook.com/wsupullman');
+		$('#customize-control-social_spot_two').find('input').attr('placeholder','https://twitter.com/wsupullman');
+		$('#customize-control-social_spot_three').find('input').attr('placeholder','https://youtube.com/washingtonstateuniv');
 		$('#customize-control-social_spot_four').find('input').attr('placeholder','http://social.wsu.edu');
 	});
 }(jQuery));
