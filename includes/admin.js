25e9201f28b7b7189020879d68ce7dd68e40ac3e
jeremyfelt <jeremy.felt@gmail.com>
2015-04-02 10:26:44 -0700

    Wrap admin.js with anonymous function, move to window.load
    $(document).on('ready') was working, but $(document).ready() was
    not. As we're waiting for JS to process a bunch of this, we can
    use window.load to ensure that things are ready

--- includes/admin.js
+++ includes/admin.js
@@ -1,15 +1,17 @@
-jQuery(document).on('ready', function() {
+(function($){
+	$(window).load(function() {
 
-	jQuery("#customize-control-spine_options-open_sans label:first-of-type input").click( function() {
+		jQuery("#customize-control-spine_options-open_sans label:first-of-type input").click( function() {
 
-		jQuery('#customize-control-spine_open_sans-400 input,#customize-control-spine_open_sans-400italic input,#customize-control-spine_open_sans-700 input').addClass('checked').prop('checked', true).prop('disabled', true);
+			jQuery('#customize-control-spine_open_sans-400 input,#customize-control-spine_open_sans-400italic input,#customize-control-spine_open_sans-700 input').addClass('checked').prop('checked', true).prop('disabled', true);
+
+		});
+
+		jQuery("#customize-control-spine_options-open_sans label:last-of-type input").click( function() {
+
+			jQuery('#customize-control-spine_open_sans-400 input,#customize-control-spine_open_sans-400italic input,#customize-control-spine_open_sans-700 input').prop('disabled', false);
+
+		});
 
 	});
-
-	jQuery("#customize-control-spine_options-open_sans label:last-of-type input").click( function() {
-
-		jQuery('#customize-control-spine_open_sans-400 input,#customize-control-spine_open_sans-400italic input,#customize-control-spine_open_sans-700 input').prop('disabled', false);
-
-	});
-
-});
+}(jQuery));
