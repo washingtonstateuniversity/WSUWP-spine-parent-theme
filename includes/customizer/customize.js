c61d1608f49e9a8ef9e82daedb09375c00415e98
jeremyfelt <jeremy.felt@gmail.com>
2015-04-02 09:57:23 -0700

    Remove unused assignment of "focused" class to customizer labels

--- includes/customizer/customize.js
+++ includes/customizer/customize.js
@@ -1,10 +1,5 @@
 (function($){
 	$(document).ready(function(){
-		var $accordion_section = $('.accordion-section label input');
-
-		$accordion_section.focusin( function() { $(this).closest('label').addClass('focused'); } );
-		$accordion_section.focusout( function() { $(this).closest('label').removeClass('focused'); } );
-
 		$('#customize-control-social_spot_one').find('input').attr('placeholder','http://www.facebook.com/wsupullman');
 		$('#customize-control-social_spot_two').find('input').attr('placeholder','http://twitter.com/wsupullman');
 		$('#customize-control-social_spot_three').find('input').attr('placeholder','http://youtube.com/washingtonstateuniv');
