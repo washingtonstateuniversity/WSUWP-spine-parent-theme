4183ff07b2e45d878a93830945836b63e1d5df60
jeremyfelt <jeremy.felt@gmail.com>
2015-04-02 10:31:27 -0700

    Alter all default Open Sans font selections more efficiently

--- includes/admin.js
+++ includes/admin.js
@@ -1,17 +1,14 @@
 (function($){
 	$(window).load(function() {
-		var $open_sans_control = $('#customize-control-spine_options-open_sans');
+		var $open_sans_control = $('#customize-control-spine_options-open_sans'),
+			$default_fonts = $('#customize-control-spine_open_sans-400 input,#customize-control-spine_open_sans-400italic input,#customize-control-spine_open_sans-700 input');
 
 		$open_sans_control.find('label:first-of-type input').on('click', function() {
-
-			jQuery('#customize-control-spine_open_sans-400 input,#customize-control-spine_open_sans-400italic input,#customize-control-spine_open_sans-700 input').addClass('checked').prop('checked', true).prop('disabled', true);
-
+			$default_fonts.addClass('checked').prop('checked', true).prop('disabled', true);
 		});
 
 		$open_sans_control.find('label:last-of-type input').on('click', function() {
-
-			jQuery('#customize-control-spine_open_sans-400 input,#customize-control-spine_open_sans-400italic input,#customize-control-spine_open_sans-700 input').prop('disabled', false);
-
+			$default_fonts.prop('disabled', false);
 		});
 
 	});
