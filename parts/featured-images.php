f127cf6d10ab81b74e5e25872bdc7f00e71dbe08
jeremyfelt <jeremy.felt@gmail.com>
2015-07-01 10:00:51 -0700

    Harden URL output for featured images

--- parts/featured-images.php
+++ parts/featured-images.php
@@ -4,12 +4,12 @@
 if ( spine_has_background_image() ) {
 	$background_image_src = spine_get_background_image_src();
 
-	?><style> #jacket { background-image: url(<?php echo esc_url( $background_image_src ); ?>); }</style><?php
+	?><style> #jacket { background-image: url('<?php echo esc_url( $background_image_src ); ?>'); }</style><?php
 }
 
 // If a featured image is assigned to the post, output it as a figure with a background image accordingly.
 if ( spine_has_featured_image() ) {
 	$featured_image_src = spine_get_featured_image_src();
 
-	?><figure class="featured-image" style="background-image: url('<?php echo $featured_image_src ?>');"><?php spine_the_featured_image(); ?></figure><?php
+	?><figure class="featured-image" style="background-image: url('<?php echo esc_url( $featured_image_src ); ?>');"><?php spine_the_featured_image(); ?></figure><?php
 }
