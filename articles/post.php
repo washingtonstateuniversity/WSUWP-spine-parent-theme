1c1afcbda0a10a79592ebb7330313584616648f6
jeremyfelt <jeremy.felt@gmail.com>
2015-12-21 12:18:19 -0800

    Initial layout and styling of social sharing on posts

--- articles/post.php
+++ articles/post.php
@@ -16,6 +16,23 @@
 			<time class="article-date" datetime="<?php echo get_the_date( 'c' ); ?>"><?php echo get_the_date(); ?></time>
 			<cite class="article-author" role="author"><?php the_author_posts_link(); ?></cite>
 		</hgroup>
+		<?php
+		$post_share_url = esc_url( get_permalink() );
+		$post_share_title = rawurlencode( spine_get_title() );
+		?>
+		<div class="social-share-bar">
+			<li class="by-facebook">
+				<a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $post_share_url; ?>"><span class="channel-title">Share on Facebook</span></a></li>
+			<li class="by-twitter">
+				<a href="https://twitter.com/intent/tweet?text=<?php echo $post_share_title; ?>&amp;url=<?php echo $post_share_url; ?>&amp;via=wsupullman" target="_blank"><span class="channel-title">Share on Twitter</span></a></li>
+			<li class="by-googleplus">
+				<a href="https://plus.google.com/share?url=<?php echo $post_share_url; ?>"><span class="channel-title">Share on Google+</span></a></li>
+			<li class="by-linkedin">
+				<a href="https://www.linkedin.com/shareArticle?mini=true&amp;url=<?php echo $post_share_url; ?>&amp;summary=<?php echo $post_share_title; ?>&amp;source=undefined" target="_blank"><span class="channel-title">Share on Linkedin</span></a></li>
+			<li class="by-email">
+				<a href="mailto:?subject=<?php echo $post_share_title; ?>&amp;body=<?php echo $post_share_url; ?>"><span class="channel-title">Email this post</span></a></li>
+			</ul>
+		</div>
 	</header>
 
 	<?php if ( ! is_singular() ) : ?>
