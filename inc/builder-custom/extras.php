c037bcc69d9ac50330081f4df7f23d3a82317018
jeremyfelt <jeremy.felt@gmail.com>
2016-02-18 14:58:33 -0800

    Remove unused ttfmake_wp_title function

--- inc/builder-custom/extras.php
+++ inc/builder-custom/extras.php
@@ -46,44 +46,6 @@ endif;
 
 //add_filter( 'body_class', 'ttfmake_body_classes' );
 
-if ( ! function_exists( 'ttfmake_wp_title' ) ) :
-	/**
-	 * Filters wp_title to print a neat <title> tag based on what is being viewed.
-	 *
-	 * @since  1.0.0.
-	 *
-	 * @param  string    $title    Default title text for current view.
-	 * @param  string    $sep      Optional separator.
-	 *
-	 * @return string              The filtered title.
-	 */
-	function ttfmake_wp_title( $title, $sep ) {
-		global $page, $paged;
-
-		if ( is_feed() ) {
-			return $title;
-		}
-
-		// Add the blog name
-		$title .= get_bloginfo( 'name' );
-
-		// Add the blog description for the home/front page.
-		$site_description = get_bloginfo( 'description', 'display' );
-		if ( $site_description && ( is_home() || is_front_page() ) ) {
-			$title .= " $sep $site_description";
-		}
-
-		// Add a page number if necessary:
-		if ( $paged >= 2 || $page >= 2 ) {
-			$title .= " $sep " . sprintf( __( 'Page %s', 'make' ), max( $paged, $page ) );
-		}
-
-		return $title;
-	}
-endif;
-
-//add_filter( 'wp_title', 'ttfmake_wp_title', 10, 2 );
-
 if ( ! function_exists( 'sanitize_hex_color' ) ) :
 	/**
 	 * Sanitizes a hex color.
