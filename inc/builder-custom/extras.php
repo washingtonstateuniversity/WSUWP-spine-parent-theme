c8a19fc20b4aa9335037aebfba9b86ac1afcd61b
jeremyfelt <jeremy.felt@gmail.com>
2016-02-18 14:59:59 -0800

    Remove unused ttfmake_page_menu_args function

--- inc/builder-custom/extras.php
+++ inc/builder-custom/extras.php
@@ -3,23 +3,6 @@
  * @package Make
  */
 
-if ( ! function_exists( 'ttfmake_page_menu_args' ) ) :
-	/**
-	 * Get our wp_nav_menu() fallback, wp_page_menu(), to show a home link.
-	 *
-	 * @since  1.0.0.
-	 *
-	 * @param  array    $args    Configuration arguments.
-	 * @return array             Modified page menu args.
-	 */
-	function ttfmake_page_menu_args( $args ) {
-		$args['show_home'] = true;
-		return $args;
-	}
-endif;
-
-//add_filter( 'wp_page_menu_args', 'ttfmake_page_menu_args' );
-
 if ( ! function_exists( 'sanitize_hex_color' ) ) :
 	/**
 	 * Sanitizes a hex color.
