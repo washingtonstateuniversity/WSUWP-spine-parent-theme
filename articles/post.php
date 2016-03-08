32e71986bf9aacfb954ef495e5d8b8688a01dd7f
Phil Cable <pcable@wsu.edu>
2016-03-08 14:49:13 -0800

    Break share tools out into a separate template

--- articles/post.php
+++ articles/post.php
@@ -37,22 +37,11 @@ if ( ! empty( $spine_social_options['twitter'] ) ) {
 			</cite>
 		</hgroup>
 
-		<?php if ( is_singular() && in_array( $post_share_placement, array( 'top', 'both' ) ) ) : ?>
-		<div class="social-share-bar">
-			<ul>
-			<li class="by-facebook">
-				<a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $post_share_url; ?>"><span class="channel-title">Share on Facebook</span></a></li>
-			<li class="by-twitter">
-				<a href="https://twitter.com/intent/tweet?text=<?php echo $post_share_title; ?>&amp;url=<?php echo $post_share_url; ?>&amp;via=<?php echo $twitter_handle; ?>" target="_blank"><span class="channel-title">Share on Twitter</span></a></li>
-			<li class="by-googleplus">
-				<a href="https://plus.google.com/share?url=<?php echo $post_share_url; ?>"><span class="channel-title">Share on Google+</span></a></li>
-			<li class="by-linkedin">
-				<a href="https://www.linkedin.com/shareArticle?mini=true&amp;url=<?php echo $post_share_url; ?>&amp;summary=<?php echo $post_share_title; ?>&amp;source=undefined" target="_blank"><span class="channel-title">Share on Linkedin</span></a></li>
-			<li class="by-email">
-				<a href="mailto:?subject=<?php echo $post_share_title; ?>&amp;body=<?php echo $post_share_url; ?>"><span class="channel-title">Email this post</span></a></li>
-			</ul>
-		</div>
-		<?php endif; ?>
+		<?php
+		if ( is_singular() && in_array( $post_share_placement, array( 'top', 'both' ) ) ) {
+			get_template_part( 'parts/share-tools' );
+		}
+		?>
 	</header>
 
 	<?php if ( ! is_singular() ) : ?>
@@ -88,22 +77,11 @@ if ( ! empty( $spine_social_options['twitter'] ) ) {
 	<?php endif; ?>
 
 	<footer class="article-footer">
-		<?php if ( is_singular() && in_array( $post_share_placement, array( 'bottom', 'both' ) ) ) : ?>
-			<div class="social-share-bar">
-				<ul>
-					<li class="by-facebook">
-						<a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $post_share_url; ?>"><span class="channel-title">Share on Facebook</span></a></li>
-					<li class="by-twitter">
-						<a href="https://twitter.com/intent/tweet?text=<?php echo $post_share_title; ?>&amp;url=<?php echo $post_share_url; ?>&amp;via=<?php echo $twitter_handle; ?>" target="_blank"><span class="channel-title">Share on Twitter</span></a></li>
-					<li class="by-googleplus">
-						<a href="https://plus.google.com/share?url=<?php echo $post_share_url; ?>"><span class="channel-title">Share on Google+</span></a></li>
-					<li class="by-linkedin">
-						<a href="https://www.linkedin.com/shareArticle?mini=true&amp;url=<?php echo $post_share_url; ?>&amp;summary=<?php echo $post_share_title; ?>&amp;source=undefined" target="_blank"><span class="channel-title">Share on Linkedin</span></a></li>
-					<li class="by-email">
-						<a href="mailto:?subject=<?php echo $post_share_title; ?>&amp;body=<?php echo $post_share_url; ?>"><span class="channel-title">Email this post</span></a></li>
-				</ul>
-			</div>
-		<?php endif; ?>
+		<?php
+		if ( is_singular() && in_array( $post_share_placement, array( 'bottom', 'both' ) ) ) {
+			get_template_part( 'parts/share-tools' );
+		}
+		?>
 	<?php
 	// Display site level categories attached to the post.
 	if ( has_category() ) {
