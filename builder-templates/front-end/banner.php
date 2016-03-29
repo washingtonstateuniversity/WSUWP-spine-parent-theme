00c82cc16aade2caf42114348e655a5e11a937a3
Phil Cable <pcable@wsu.edu>
2016-03-29 15:25:44 -0700

    Use Yoda condition per PHPCS

--- builder-templates/front-end/banner.php
+++ builder-templates/front-end/banner.php
@@ -111,7 +111,7 @@ if ( '' === $section_id ) {
 
 		<div class="builder-section-content<?php echo ( $is_slider ) ? ' cycle-slideshow' : ''; ?>"<?php echo ( $is_slider ) ? ttfmake_builder_get_banner_slider_atts( $ttfmake_section_data ) : ''; ?>>
 			<?php if ( ! empty( $banner_slides ) ) : $i = 0; foreach ( $banner_slides as $slide ) : ?>
-				<div class="builder-banner-slide<?php echo ttfmake_builder_banner_slide_class( $slide ); echo ( $i++ == 0 ) ? ' first-slide' : ''; ?>" style="<?php echo ttfmake_builder_banner_slide_style( $slide, $ttfmake_section_data ); ?>">
+				<div class="builder-banner-slide<?php echo ttfmake_builder_banner_slide_class( $slide ); echo ( 0 == $i++ ) ? ' first-slide' : ''; ?>" style="<?php echo ttfmake_builder_banner_slide_style( $slide, $ttfmake_section_data ); ?>">
 					<?php if ( ! empty( $slide['slide-url'] ) ) : ?><a href="<?php echo esc_url( $slide['slide-url'] ); ?>"><?php endif; ?>
 					<div class="builder-banner-content">
 						<?php if ( ! empty( $slide['slide-title'] ) ) : ?>
