a3c510b7c18ca26e19bb8e343dbd91c70c7e9e12
Jeremy Felt <jeremy.felt@gmail.com>
2016-05-04 11:04:08 -0700

    Bump version 0.26.5

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 // Global version tracker.
-$wsuwp_spine_theme_version = '0.26.4';
+$wsuwp_spine_theme_version = '0.26.5';
 
 include_once( 'includes/theme-setup.php' ); // Setup basic portions of the theme.
 include_once( 'includes/theme-navigation.php' ); // Include functionality for navigation.
