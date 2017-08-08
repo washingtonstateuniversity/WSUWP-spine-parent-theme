78b01c403187156fa1ce8d6e2bff70a37a6243de
Phil Cable <pcable@wsu.edu>
2017-08-08 14:13:55 -0700

    Output contact meta tags only when their respective options have value
    This is probably a bit overboard, but it does avoid empty divs from
    being output in the Spine contact tab.

--- parts/contact.php
+++ parts/contact.php
@@ -5,12 +5,22 @@
 	<meta itemprop="url" class="required" content="<?php echo esc_attr( spine_get_option( 'contact_url' ) ); ?>">
 	<?php endif; ?>
 	<div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+		<?php if ( spine_get_option( 'contact_streetAddress' ) !== '' ) : ?>
 		<meta itemprop="streetAddress" class="optional" content="<?php echo esc_attr( spine_get_option( 'contact_streetAddress' ) ); ?>">
+		<?php endif; ?>
+		<?php if ( spine_get_option( 'contact_addressLocality' ) !== '' ) : ?>
 		<meta itemprop="addressLocality" class="optional" content="<?php echo esc_attr( spine_get_option( 'contact_addressLocality' ) ); ?>">
+		<?php endif; ?>
+		<?php if ( spine_get_option( 'contact_postalCode' ) !== '' ) : ?>
 		<meta itemprop="postalCode" class="required" content="<?php echo esc_attr( spine_get_option( 'contact_postalCode' ) ); ?>">
+		<?php endif; ?>
 	</div>
+	<?php if ( spine_get_option( 'contact_telephone' ) !== '' ) : ?>
 	<meta itemprop="telephone" class="required" content="<?php echo esc_attr( spine_get_option( 'contact_telephone' ) ); ?>">
+	<?php endif; ?>
+	<?php if ( spine_get_option( 'contact_email' ) !== '' ) : ?>
 	<meta itemprop="email" class="required" content="<?php echo esc_attr( spine_get_option( 'contact_email' ) ); ?>">
+	<?php endif; ?>
 	<?php
 	$contact_point = spine_get_option( 'contact_ContactPoint' );
 	if ( ! empty( $contact_point ) ) {
