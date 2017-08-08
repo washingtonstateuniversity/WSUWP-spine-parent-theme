b163cc66491ad7b40e33ee217b685eaeb44ac781
Jeremy Felt <jeremy.felt@gmail.com>
2017-08-07 17:26:08 -0700

    Replace CSS Lint with Stylelint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,10 @@
-var Promise = require('es6-promise').polyfill();
-
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+
+	    stylelint: {
+		    src: [ "src/*.css" ]
+	    },
 
         concat: {
             options: {
@@ -30,42 +32,6 @@ module.exports = function(grunt) {
             }
         },
 
-        csslint: {
-            main: {
-                src: [ "style.css" ],
-                options: {
-                    "adjoining-classes": false,
-                    "box-sizing": false,                  // unless we want to support IE7
-                    "compatible-vendor-prefixes": false,  // The library on this is older than autoprefixer.
-                    "fallback-colors": false,             // unless we want to support IE8
-                    "font-sizes": false,                  // audit
-                    "gradients": false,                   // The library on this is older than autoprefixer.
-                    "ids": false,
-                    "important": false,                   // This should be set to 2 one day.
-                    "order-alphabetical": false,
-                    "overqualified-elements": false,      // We have weird uses that will always generate warnings.
-                    "qualified-headings": false,
-                    "known-properties": 1,                // Okay to ignore in the case of known unknowns.
-                    "box-model": 2,
-                    "display-property-grouping": 2,
-                    "duplicate-background-images": 2,
-                    "duplicate-properties": 2,
-                    "empty-rules": 2,
-                    "floats": 2,
-                    "outline-none": 2,
-                    "regex-selectors": 2,
-                    "shorthand": 2,
-                    "star-property-hack": 2,
-                    "text-indent": 2,
-                    "unique-headings": 2,
-                    "universal-selector": 2,
-                    "unqualified-attributes": 2,
-                    "vendor-prefix": 2,
-                    "zero-units": 2
-                }
-            }
-        },
-
         clean: {
             options: {
                 force: true
@@ -84,12 +50,12 @@ module.exports = function(grunt) {
         }
     });
 
-    grunt.loadNpmTasks( "grunt-postcss" );
-    grunt.loadNpmTasks( "grunt-contrib-concat" );
-    grunt.loadNpmTasks( "grunt-contrib-csslint" );
-    grunt.loadNpmTasks( "grunt-contrib-clean" );
-    grunt.loadNpmTasks( "grunt-phpcs" );
+	grunt.loadNpmTasks( "grunt-postcss" );
+	grunt.loadNpmTasks( "grunt-contrib-concat" );
+	grunt.loadNpmTasks( "grunt-contrib-clean" );
+	grunt.loadNpmTasks( "grunt-phpcs" );
+	grunt.loadNpmTasks( "grunt-stylelint" );
 
     // Default task(s).
-    grunt.registerTask('default', ['concat', 'postcss', 'csslint', 'clean', 'phpcs' ]);
+    grunt.registerTask('default', [ 'stylelint', 'concat', 'postcss', 'clean', 'phpcs' ]);
 };
