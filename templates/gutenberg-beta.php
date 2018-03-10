0a8993a642be2c56773db817e98a3ff988f25758
Jeremy Felt <jeremy.felt@gmail.com>
2018-03-09 16:30:18 -0800

    Adjust markup on Gutenberg page template

--- templates/gutenberg-beta.php
+++ templates/gutenberg-beta.php
@@ -11,14 +11,29 @@ get_header();
 ?>
 <main id="wsuwp-main" class="spine-gutenberg-beta-template">
 
-	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+	<?php get_template_part( 'parts/featured-images' ); ?>
 
-		<?php the_content(); ?>
+	<header class="row single gutter pad-ends">
+		<h1 class="column one"><?php the_title(); ?></h1>
+	</header>
 
-	<?php
-	endwhile;
-	endif;
-	?>
+	<section class="row single gutter pad-bottom">
+
+		<div class="column one">
+
+			<?php
+			if ( have_posts() ) : while ( have_posts() ) : the_post();
+
+				the_content();
+
+			endwhile;
+			endif;
+
+			?>
+		</div>
+
+	</section>
+
 </main>
 <?php
 get_footer();
