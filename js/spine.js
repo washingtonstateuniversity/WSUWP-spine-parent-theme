6e2bdde63c31e22d006502f03ad36c9e263989d0
Adam Turner <act@adamturner.dev>
2021-06-21 10:34:17 -0700

    Replace Spine search UI with hardcoded link to WSU search page

--- js/spine.js
+++ js/spine.js
@@ -1,50 +1,3 @@
-/*
-	TermTemplate:"<strong> <%this.term%> </strong>",
-	this.options.relatedHeader
-
-	termTemplate
-	autoSearchObj.options.provider.termTemplate
-*/
-
-/* jshint onevar: false */
-( function( $ ) {
-	"use strict";
-	$.widget( "ui.autosearch", $.ui.autocomplete, {
-		_renderMenu: function( ul, items ) {
-			var that	= this;
-			var related	= $.grep( items, function( obj ) {//, item) {
-				return obj.related !== "false";
-			} );
-			var unrelated = $.grep( items, function( obj ) {//, item) {
-				return obj.related === "false";
-			} );
-			$.each( unrelated, function( i, item ) {
-				that._renderItemData( ul, item );
-			} );
-
-			if ( this.options.showRelated && related.length ) {
-				if ( this.options.relatedHeader ) {
-					that._renderHeader( ul, this.options.relatedHeader );
-				}
-				$.each( related, function( i, item ) {
-					that._renderItemData( ul, item );
-				} );
-			}
-		},
-		_renderItemData: function( ul, item ) {
-			return this._renderItem( ul, item ).data( "ui-autocomplete-item", item );
-		},
-		_renderItem: function( ul, item ) {
-			var text	= item.label;
-
-			return $( "<li></li>" ).data( "item.autocomplete", item ).append( text ).appendTo( ul );
-		},
-		_renderHeader: function( ul, text ) {
-			return $( "<li></li>" ).append( "<a href=''>" + text + "</a>" ).appendTo( ul );
-		}
-	} );
-} )( jQuery );
-
 /** Intended usage is
 *	$.spine({
 *		"option":"value"
@@ -355,7 +308,6 @@
 			this.instance = {
 				spine: self.options,
 				framework: [],
-				search: [],
 				social: []
 			};
 
@@ -368,7 +320,7 @@
 		 * Note: Context is not yet implemented.
 		 *
 		 * @param {object} obj     e.g. {'foo':'bar'}
-		 * @param {string} context e.g. 'search', 'social', 'framework'
+		 * @param {string} context e.g. 'social', 'framework'
 		 */
 		_set_globals: function( obj, context ) {
 			context = null; // Avoiding jshint error temporarily.
@@ -382,7 +334,7 @@
 		/**
 		 * Retrieve a context's objects from the global spine object.
 		 *
-		 * @param {string} context e.g. 'search', 'social', 'framework'
+		 * @param {string} context e.g. 'social', 'framework'
 		 * @returns {*}
 		 * @private
 		 */
@@ -393,7 +345,7 @@
 		/**
 		 * Clears an object of a context.
 		 *
-		 * @param {string} context e.g. 'search', 'social', 'framework'
+		 * @param {string} context e.g. 'social', 'framework'
 		 */
 		clear: function( context ) {
 			this._c( this.get( context ) );
@@ -418,7 +370,7 @@
 		/**
 		 * Returns objects with a specific context.
 		 *
-		 * @param {string} context In what context, e.g. 'search', 'social', 'framework'
+		 * @param {string} context In what context, e.g. 'social', 'framework'
 		 * @param {object} options Contains string property, string value, string operator (AND/OR).
 		 * @param callback:function(search:jObj, isFound:boolean)
 		 */
@@ -506,14 +458,14 @@
 		 * Destroys spine elements and options.
 		 */
 		clear_spine: function() {
-			this.clear( "search" ).clear( "framework" ).clear( "social" );
+			this.clear( "framework" ).clear( "social" );
 		},
 
 		/**
 		 * Destroys the plugin.
 		 */
 		destroy: function( callback ) {
-			this.clear( "search" ).clear( "framework" ).clear( "social" )._c( this.instance );
+			this.clear( "framework" ).clear( "social" )._c( this.instance );
 			$.removeData( this.el, this.name );
 			this._call( callback, this );
 		}
@@ -537,7 +489,7 @@
 			// Initialize the Spine plugin.
 			targ.spine( {} );
 
-			options = $.extend( { "framework":{}, "search":{}, "social":{} }, options );
+			options = $.extend( { "framework":{}, "social":{} }, options );
 
 			// Setup each of the extensions.
 			$.each( options, function( i, v ) {
@@ -1339,254 +1291,6 @@
 			$( "button.shut" ).on( "click touchend", function( e ) {
 				e.preventDefault();
 				wsu_actions.find( ".opened" ).toggleClass( "opened closed" );
-			} );
-		}
-	} );
-}( jQuery ) );
-
- /*!
- *
- * Depends:
- *		jquery.ui.v.js
- */
-/*jshint multistr: true */
-( function( $ ) {
-	"use strict";
-	$.extend( $.ui.spine.prototype, {
-		search_init: function( options ) {
-			var self;
-			self = this;//Hold to preserve scop
-			$.extend( options, self.search_options, options );
-			this._set_globals( this.search_globals );
-			this.create_search();
-		},
-
-		search_options:{
-			data:[],
-			providers:{
-				nav:{
-					name:"From Navigation",
-					nodes: ".spine-navigation",
-					dataType: "html",
-					maxRows: 12,
-					urlSpaces:"%20"
-				},
-				atoz:{
-					name:"WSU A to Z index",
-					url: "https://search.wsu.edu/2013service/searchservice/search.asmx/AZSearch",
-					urlSpaces:"+",
-					dataType: "jsonp",
-					featureClass: "P",
-					style: "full",
-					maxRows: 12,
-					termTemplate:"<strong><%this.term%></strong>",
-					resultTemplate:""
-				}
-			},
-			search:{
-				minLength: 2,
-				maxRows: 12,
-				getRelated:true,
-				urlSpaces:"+",
-				tabTemplate: "<section id='wsu-search' class='spine-search spine-action closed'>" +
-								"<form id='default-search'>" +
-									"<input name='term' type='text' value='' placeholder='search' title='Type search term here'>" +
-									"<button type='submit'>Submit</button>" +
-								"</form>" +
-								"<div id='spine-shortcuts' class='spine-shortcuts'></div>" +
-							"</section>"
-			},
-			result:{
-				appendTo: "#spine-shortcuts",
-				showRelated:true,
-				target:"_blank",
-				relatedHeader:"<b class='related_sep'>Related</b><hr/>",
-				providerHeader:"<b class='provider_header'><%this.provider_name%></b><hr/>",
-				termTemplate:"<b><%this.term%></b>",
-				template:"<li><%this.searchitem%></li>"
-			}
-		},
-		search_globals: {
-			wsu_search: $( "#wsu-search" ),
-			search_input:$( "#wsu-search input[type=text]" )
-		},
-		create_search: function() {
-			var self, wsu_search, tabhtml;
-			self = this;//Hold to preserve scop
-			wsu_search = self._get_globals( "wsu_search" ).refresh();
-			if ( !wsu_search.length ) {
-				tabhtml = $.runTemplate( self.search_options.search.tabTemplate, {} );
-			}else {
-				tabhtml = "<section id='wsu-search' class='spine-search spine-action closed'>" + wsu_search.html() + "</section>";
-				wsu_search.remove();
-			}
-			this.setup_tabs( "search", tabhtml );
-
-			if ( $( "#spine-shortcuts" ).length <= 0 ) {
-				$( "#wsu-search" ).append( "<div id='spine-shortcuts' class='spine-shortcuts'></div>" );
-			}
-
-			$( "#wsu-search-tab button" ).on( "click touchend", function() {
-				self._get_globals( "search_input" ).refresh().trigger( "focus" );
-			} );
-			this.setup_search();
-		},
-
-		start_search:function( request, callback ) {
-			var self, term, queries = [];
-			self = this;//Hold to preserve scop
-
-			term = $.trim( request.term );
-			self.search_options.data = [];
-			$.each( self.search_options.providers, function( i, provider ) {
-				$.ui.autocomplete.prototype.options.termTemplate = ( typeof( provider.termTemplate ) !== undefined && provider.termTemplate !== "" ) ? provider.termTemplate : undefined;
-				queries.push( self.run_query( term, provider ) );
-			} );
-
-			$.when.apply( $, queries ).done(
-			function() {
-				$.each( arguments, function( i, v ) {
-					var data, proData;
-					if ( v !== undefined ) {
-						data = v[ 0 ];
-						if ( data !== undefined && data.length > 0 ) {
-							proData = self.setup_result_obj( term, data );
-							$.merge( self.search_options.data, proData );
-						}
-					}
-				} );
-				self._call( callback, self.search_options.data );
-			} );
-		},
-
-		run_query:function( term, provider ) {
-			var self, result = [], tmpObj = [], nodes;
-			self = this;//Hold to preserve scop
-			result = [];
-
-			if ( typeof( provider ) !== undefined && typeof( provider.url ) !== undefined && provider.nodes === undefined ) {
-				return $.ajax( {
-					url: provider.url,
-					dataType: provider.dataType,
-					data: {
-						featureClass: provider.featureClass,
-						style: provider.style,
-						maxRows: provider.maxRows,
-						name_startsWith: term,
-						related:self.search_options.search.getRelated
-					}
-				} );
-			}else if ( typeof( provider ) !== undefined && typeof( provider.nodes ) !== undefined ) {
-				nodes = $( provider.nodes ).find( "a" );
-				$.each( nodes, function( i, v ) {
-					var obj, text, localtmpObj;
-					obj = $( v );
-					text = obj.text();
-					if ( text.toLowerCase().indexOf( term.toLowerCase() ) > -1 && obj.attr( "href" ) !== "#" ) {
-						localtmpObj = {
-							label:text,
-							value:obj.attr( "href" ),
-							related:"false",
-							searchKeywords:""
-						};
-						tmpObj.push( localtmpObj );
-					}
-				} );
-				return [ tmpObj ];
-			}
-		},
-
-		format_result_text:function( term, text, value ) {
-			var self, termTemplate, regex;
-			self = this;//Hold to preserve scope
-
-			termTemplate = "<strong>$1</strong>"; //Typeof($.ui.autocomplete.prototype.options.termTemplate)!==undefined ? $.ui.autocomplete.prototype.options.termTemplate : "<strong>$1</strong>";
-
-			regex	= "(?![^&;]+;)(?!<[^<>]*)(" + $.ui.autocomplete.escapeRegex( term ) + ")(?![^<>]*>)(?![^&;]+;)";
-			text	= "<a href='" + value + "' target='" + self.search_options.result.target + "'>" + text.replace( new RegExp( regex, "gi" ), termTemplate ) + "</a>";
-
-			return text;
-		},
-
-		setup_result_obj:function( term, data ) {
-			var self, matcher;
-			self = this;//Hold to preserve scop
-			matcher = new RegExp( $.ui.autocomplete.escapeRegex( term ), "i" );
-			return $.map( data, function( item ) {
-				var text, value, resultObj;
-				text = item.label;
-				value	= item.value;
-				if ( ( item.value && ( !term || matcher.test( text ) ) || item.related === "true" ) ) {
-					text = self.format_result_text( term, text, value );
-					resultObj = {
-						label: text,
-						value: item.value,
-						searchKeywords: item.searchKeywords !== undefined ? item.searchKeywords : "false",
-						related: item.related !== undefined ? item.related : "false"
-					};
-					return resultObj;
-				}
-			} );
-		},
-
-		setup_search: function() {
-			var self, wsu_search, search_input, focuseitem = {};
-
-			self = this;//Hold to preserve scop
-			wsu_search = self._get_globals( "wsu_search" ).refresh();
-			search_input = self._get_globals( "search_input" ).refresh();
-			focuseitem = {};
-
-			search_input.autosearch( {
-
-				appendTo:			self.search_options.result.appendTo,
-				showRelated:		self.search_options.result.showRelated,
-				relatedHeader:		self.search_options.result.relatedHeader,
-				minLength:			self.search_options.search.minLength,
-
-				source: function( request, response )  {
-					self.start_search( request, function( data ) {
-						response( data );
-					} );
-				},
-				search: function( ) {
-					focuseitem = {};
-				},
-				select: function( e, ui ) {
-					var id, term;
-					id = ui.item.searchKeywords;
-					term = $( ui.item.label ).text();
-					search_input.val( term );
-					search_input.autosearch( "close" );
-					return false;
-				},
-				focus: function( e, ui ) {
-					search_input.val( $( ui.item.label ).text() );
-					focuseitem = {
-						label:ui.item.label
-					};
-					e.preventDefault();
-				},
-				open: function( ) {},
-				close: function( e ) {
-					e.preventDefault();
-					return false;
-				}
-			} ).data( "autosearch" );
-
-			$( "#wsu-search form" ).on( "submit", function() {
-				var scope, site, cx, cof, search_term, search_url;
-				scope = wsu_search.attr( "data-default" );
-				site = " site:" + window.location.hostname;
-				if ( scope === "wsu.edu" ) {
-					site = "";
-				}
-				cx = "cx=004677039204386950923:xvo7gapmrrg";
-				cof = "cof=FORID%3A11";
-				search_term = search_input.val();
-				search_url = "https://search.wsu.edu/default.aspx?" + cx + "&" + cof + "&q=" + search_term + site;
-				window.location.href = search_url;
-				return false;
 			} );
 		}
 	} );
--- spine/header.php
+++ spine/header.php
@@ -5,7 +5,7 @@
 <section id="wsu-actions" class="spine-actions clearfix">
 
 	<ul id="wsu-actions-tabs" class="spine-actions-tabs spine-tabs clearfix">
-		<li id="wsu-search-tab" class="spine-search-tab closed"><button>Search</button></li>
+		<li id="wsu-search-tab" class="spine-search-tab closed"><a href="https://search.wsu.edu/">Search WSU</a></li>
 		<li id="wsu-contact-tab" class="spine-contact-tab closed"><button>Contact</button></li>
 		<li id="wsu-share-tab" class="spine-share-tab closed"><button>Share</button></li>
 	</ul>
